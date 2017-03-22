39ee77351873b256fe1ac898b14b4e2f34dedafc
jishilei <jishilei@baidu.com>
2017-03-22 21:21:55 +0800

    schema desc
    Change-Id: I8c7678f5ce0fc031daaf15f2be4c57d41d34260c

--- hugegraph-core/src/main/java/com/baidu/hugegraph2/util/StringUtil.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph2/util/StringUtil.java
@@ -5,8 +5,8 @@ package com.baidu.hugegraph2.util;
  */
 public class StringUtil {
     public static String captureName(String name) {
-        char[] cs=name.toCharArray();
-        cs[0]-=32;
+        char[] cs = name.toCharArray();
+        cs[0] -= 32;
         return String.valueOf(cs);
 
     }
