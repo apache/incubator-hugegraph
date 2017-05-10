5c52abf75257266f26565416cd6838bdc3d3d25a
liningrui <liningrui@baidu.com>
2017-05-10 11:46:23 +0800

    add some init script temporyly
    Change-Id: If6f51f0c44f9a2ccdd9cd26a341f27fe0ed35916

--- hugegraph-dist/src/assembly/static/scripts/empty-sample.groovy
+++ hugegraph-dist/src/assembly/static/scripts/empty-sample.groovy
@@ -1,3 +1,9 @@
+import com.baidu.hugegraph.schema.SchemaManager
+import com.baidu.hugegraph.type.schema.EdgeLabel
+import com.baidu.hugegraph.type.schema.VertexLabel
+import org.apache.tinkerpop.gremlin.structure.T
+import org.apache.tinkerpop.gremlin.structure.Vertex
+
 // an init script that returns a Map allows explicit setting of global bindings.
 def globals = [:]
 
@@ -6,6 +12,48 @@ def globals = [:]
 globals << [hook: [
         onStartUp : { ctx ->
             ctx.logger.info("Executed once at startup of Gremlin Server.")
+            // TODO: Should remove when init.sh been added.
+            graph.initBackend()
+
+            schema = graph.schema()
+
+            schema.makePropertyKey("name").asText().create()
+            schema.makePropertyKey("age").asInt().create()
+            schema.makePropertyKey("lang").asText().create()
+            schema.makePropertyKey("date").asText().create()
+
+            person = schema.makeVertexLabel("person").properties("name", "age").primaryKeys("name").create()
+
+            software = schema.makeVertexLabel("software").properties("name", "lang").primaryKeys("name").create()
+
+            schema.makeIndex("personByName").on(person).by("name").secondary().create()
+
+            schema.makeIndex("softwareByName").on(software).by("name").search().create()
+            schema.makeIndex("softwareByLang").on(software).by("lang").search().create()
+
+            schema.makeEdgeLabel("knows").link("person", "person").properties("date").create()
+
+            EdgeLabel created = schema.makeEdgeLabel("created").link("person", "software").properties("date").create()
+
+            schema.makeIndex("createdByDate").on(created).by("date").secondary().create()
+            
+            graph.tx().open()
+            
+            marko = graph.addVertex(T.label, "person", "name", "marko", "age", 29)
+            vadas = graph.addVertex(T.label, "person", "name", "vadas", "age", 27)
+            lop = graph.addVertex(T.label, "software", "name", "lop", "lang", "java")
+            josh = graph.addVertex(T.label, "person", "name", "josh", "age", 32)
+            ripple = graph.addVertex(T.label, "software", "name", "ripple", "lang", "java")
+            peter = graph.addVertex(T.label, "person", "name", "peter", "age", 35)
+
+            marko.addEdge("knows", vadas, "date", "20160110")
+            marko.addEdge("knows", josh, "date", "20130220")
+            marko.addEdge("created", lop, "date", "20171210")
+            josh.addEdge("created", ripple, "date", "20171210")
+            josh.addEdge("created", lop, "date", "20091111")
+            peter.addEdge("created", lop, "date", "20170324")
+
+            graph.tx().commit()
         },
         onShutDown: { ctx ->
             ctx.logger.info("Executed once at shutdown of Gremlin Server.")
