74c86711c581679d3ac7440eb96f0e208c1752eb
Zhangmei Li <lizhangmei@baidu.com>
2017-05-18 17:55:03 +0800

    HugeGraph-81: remove some unused imports
    Change-Id: Ib7a8a0f11657f92bc0c28b94917a681a1fe5fa3a

--- hugegraph-dist/src/main/java/com/baidu/hugegraph/server/HugeGraphServer.java
+++ hugegraph-dist/src/main/java/com/baidu/hugegraph/server/HugeGraphServer.java
@@ -1,21 +1,6 @@
 package com.baidu.hugegraph.server;
 
-import static com.baidu.hugegraph.configuration.ConfigSpace.BACKEND;
-import static com.baidu.hugegraph.configuration.ConfigSpace.SERIALIZER;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.tree.ConfigurationNode;
-import org.apache.tinkerpop.gremlin.server.GremlinServer;
-import org.apache.tinkerpop.gremlin.util.config.YamlConfiguration;
-
 import com.baidu.hugegraph.HugeException;
-import com.baidu.hugegraph.backend.serializer.SerializerFactory;
-import com.baidu.hugegraph.backend.store.BackendProviderFactory;
-import com.baidu.hugegraph.configuration.HugeConfiguration;
 
 /**
  * Created by liningrui on 2017/5/10.
