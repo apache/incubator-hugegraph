a7e3b5d395b0f659b4433fd127217f18d28d46aa
Zhangmei Li <lizhangmei@baidu.com>
2017-05-25 13:52:23 +0800

    HugeGraph-143: add some exception tests(vertex)
    Change-Id: If38094db8353bcbcfd520c43aab212e9c0ae53af

--- hugegraph-test/src/main/java/com/baidu/hugegraph/core/Utils.java
+++ hugegraph-test/src/main/java/com/baidu/hugegraph/core/Utils.java
@@ -1,8 +1,10 @@
 package com.baidu.hugegraph.core;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.junit.Assert;
 
 import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.core.FakeObjects.FakeVertex;
@@ -26,5 +28,37 @@ public class Utils {
             }
         }
         return false;
+    }
+
+    @FunctionalInterface
+    interface ThrowableRunnable {
+        void run() throws Throwable;
+    }
+
+    public static void assertThrows(
+            Class<? extends Throwable> throwable,
+            ThrowableRunnable runnable) {
+        assertThrows(throwable, runnable, t -> {});
+    }
+
+    public static void assertThrows(
+            Class<? extends Throwable> throwable,
+            ThrowableRunnable runnable,
+            Consumer<Throwable> exceptionConsumer) {
+        boolean fail = false;
+        try {
+            runnable.run();
+            fail = true;
+        } catch (Throwable t) {
+            Assert.assertTrue(String.format(
+                    "Bad exception type %s(expect %s)",
+                    t.getClass(), throwable),
+                    throwable.isInstance(t));
+            exceptionConsumer.accept(t);
+        }
+        if (fail) {
+            Assert.fail(String.format("No exception was thrown(expect %s)",
+                    throwable));
+        }
     }
 }
