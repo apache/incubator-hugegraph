ba70b63b180da93709891358e6e88b2505dbd0f4
zhangsuochao <zhangsuochao@baidu.com>
2017-05-26 20:08:59 +0800

    HugeGraph-112:Enable CI features

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+export MAVEN_HOME=/home/scmtools/buildkit/maven/apache-maven-3.3.9/
+export JAVA_HOME=/home/scmtools/buildkit/java/jdk1.8.0_25/
+export PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH
+mvn clean package -DskipTests
