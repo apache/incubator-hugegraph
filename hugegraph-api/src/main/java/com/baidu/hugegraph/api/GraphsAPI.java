0d32593686de145e1fa19283adbbdaf6b12363cb
liningrui <liningrui@baidu.com>
2017-06-08 14:38:23 +0800

    HugeGraph-232: provide a interface to fetch graph conf file.
    Change-Id: I841ec6c7dd40a98e33a394e64009c5791d5d6ed6

--- hugegraph-api/src/main/java/com/baidu/hugegraph/api/GraphsAPI.java
+++ hugegraph-api/src/main/java/com/baidu/hugegraph/api/GraphsAPI.java
@@ -1,10 +1,14 @@
 package com.baidu.hugegraph.api;
 
+import java.io.File;
+
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
@@ -21,6 +25,8 @@ import com.google.common.collect.ImmutableMap;
 public class GraphsAPI extends API {
 
     private static final Logger logger = LoggerFactory.getLogger(HugeServer.class);
+
+    private static final String TOKEN = "162f7848-0b6d-4faf-b557-3a0797869c55";
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -39,4 +45,26 @@ public class GraphsAPI extends API {
         return ImmutableMap.of("name", g.name());
     }
 
+    @GET
+    @Path("{name}/conf")
+    @Produces(MediaType.APPLICATION_JSON)
+    public File getConf(@Context GraphManager manager,
+                        @PathParam("name") String name,
+                        @QueryParam("token") String token) {
+        logger.debug("Graphs [{}] get graph by name '{}'", name);
+
+        if (!verifyToken(token)) {
+            throw new NotAuthorizedException("Invalid token");
+        }
+
+        HugeGraph g = (HugeGraph) graph(manager, name);
+        return g.configuration().getFile();
+    }
+
+    private boolean verifyToken(String token) {
+        if (token != null && token.equals(TOKEN)) {
+            return true;
+        }
+        return false;
+    }
 }
