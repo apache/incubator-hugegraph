036effffd600643262893e7ea61435aad066a05b
Zhangmei Li <lizhangmei@baidu.com>
2017-06-14 22:00:58 +0800

    HugeGraph-46: implement schema-cache
    Change-Id: I1d5778acee39f1ed3aa09feb9b63171a54d4845a

---
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/backend/cache/Cache.java
@@ -0,0 +1,22 @@
+package com.baidu.hugegraph.backend.cache;
+
+import java.util.function.Function;
+
+import com.baidu.hugegraph.backend.id.Id;
+
+public interface Cache {
+
+    public Object get(Id id);
+
+    public Object getOrFetch(Id id, Function<Id, Object> fetcher);
+
+    public void update(Id id, Object value);
+
+    public void updateIfAbsent(Id id, Object value) ;
+
+    public void invalidate(Id id);
+
+    public void clear();
+
+    public long size();
+}
---
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/backend/cache/RamCache.java
@@ -0,0 +1,136 @@
+package com.baidu.hugegraph.backend.cache;
+
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.baidu.hugegraph.backend.id.Id;
+
+public class RamCache implements Cache {
+
+    private static final Logger logger = LoggerFactory.getLogger(
+            RamCache.class);
+
+    private static int MB = 1024 * 1024;
+    private static int DEFAULT_SIZE = 1 * MB;
+    private static int MAX_INIT_CAP = 100 * MB;
+
+    private long hits = 0;
+    private long miss = 0;
+    private long size = 0;
+
+    // implement LRU cache
+    private Map<Id, Object> store;
+    private Queue<Id> sortedIds;
+
+    public RamCache() {
+        this(DEFAULT_SIZE);
+    }
+
+    public RamCache(long size) {
+        if (size < 8) {
+            size = 8;
+        }
+        this.size = size;
+
+        long cap = size >> 3;
+        if (cap > MAX_INIT_CAP) {
+            cap = MAX_INIT_CAP;
+        }
+        // NOTE: maybe we can use LinkedHashMap if not support multi-thread
+        this.store = new ConcurrentHashMap<>((int) cap);
+        this.sortedIds = new ConcurrentLinkedQueue<>();
+    }
+
+    private void access(Id id) {
+        // add to tail
+        this.sortedIds.remove(id);
+        this.sortedIds.add(id);
+    }
+
+    private void write(Id id, Object value) {
+        assert id != null;
+        if (this.sortedIds.size() >= this.size) {
+            // remove the oldest
+            Id removed = this.sortedIds.poll();
+            assert removed != null;
+            this.store.remove(removed);
+            logger.debug("RamCache replace '{}' with '{}' (size={})",
+                    removed, id, this.size);
+        }
+        // add the new item to tail
+        this.sortedIds.add(id);
+        this.store.put(id, value);
+    }
+
+    @Override
+    public Object get(Id id) {
+        Object value = this.store.get(id);
+        if (value != null) {
+            logger.debug("RamCache cached '{}' (hits={}, miss={})",
+                    id, ++this.hits, this.miss);
+            this.access(id);
+        } else {
+            logger.debug("RamCache missed '{}' (miss={}, hits={})",
+                    id, ++this.miss, this.hits);
+        }
+        return value;
+    }
+
+    @Override
+    public Object getOrFetch(Id id, Function<Id, Object> fetcher) {
+        Object value = this.store.get(id);
+        if (value != null) {
+            logger.debug("RamCache cached '{}' (hits={}, miss={})",
+                    id, ++this.hits, this.miss);
+            this.access(id);
+        } else {
+            logger.debug("RamCache missed '{}' (miss={}, hits={})",
+                    id, ++this.miss, this.hits);
+            value = fetcher.apply(id);
+            this.update(id, value);
+        }
+        return value;
+    }
+
+    @Override
+    public void update(Id id, Object value) {
+        if (value == null) {
+            return;
+        }
+        this.write(id, value);
+    }
+
+    @Override
+    public void updateIfAbsent(Id id, Object value) {
+        if (value == null || this.store.containsKey(id)) {
+            return;
+        }
+        this.write(id, value);
+    }
+
+    @Override
+    public void invalidate(Id id) {
+        this.store.remove(id);
+    }
+
+    @Override
+    public void clear() {
+        this.store.clear();
+    }
+
+    @Override
+    public long size() {
+        return this.size;
+    }
+
+    @Override
+    public String toString() {
+        return this.store.toString();
+    }
+}
