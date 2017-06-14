9013f97e74c851dc4ed999fa12348fef10f8db1f
liningrui <liningrui@baidu.com>
2017-06-14 18:11:56 +0800

    HugeGraph-218: fixed bug the that check connection method will ping and log error host when user change config option.
    Change-Id: I4146a64734838de7d8c9e739113f574e2868f8ca

--- hugegraph-api/bin/start-hugeserver.sh
+++ hugegraph-api/bin/start-hugeserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-abs_path() {
+function abs_path() {
     SOURCE="${BASH_SOURCE[0]}"
     while [ -h "$SOURCE" ]; do
         DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -10,50 +10,60 @@ abs_path() {
     echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 }
 
-BIN=`abs_path`
-HUGESERVER_CONFIG_TAG=huge-server
-: ${HSRV_STARTUP_TIMEOUT_S:=20}
-: ${HSRV_SHUTDOWN_TIMEOUT_S:=20}
-: ${HSRV_IP:=127.0.0.1}
-: ${HSRV_PORT:=8080}
+#read a property from a .properties file
+function read_property(){
+    # replace "." to "\."
+    propertyName=`echo $2 | sed 's/\./\\\./g'`
+    # file path
+    fileName=$1;
+    cat $fileName | sed -n -e "s/^[ ]*//g;/^#/d;s/^$propertyName=//p" | tail -1
+}
 
-: ${SLEEP_INTERVAL_S:=2}
+BIN=`abs_path`
+
+HSRV_URL=`read_property "$BIN/../conf/huge-server.properties" "hugeserver.url"`
+
+HSRV_STARTUP_TIMEOUT_S=20
+HSRV_SHUTDOWN_TIMEOUT_S=20
+SLEEP_INTERVAL_S=2
+
 VERBOSE=
 
 # wait_for_startup friendly_name host port timeout_s
-wait_for_startup() {
-    local friendly_name="$1"
-    local host="$2"
-    local port="$3"
-    local timeout_s="$4"
+function wait_for_startup() {
+
+    local server_name="$1"
+    local server_url="$2"
+    local timeout_s="$3"
 
     local now_s=`date '+%s'`
     local stop_s=$(( $now_s + $timeout_s ))
-    local status=
 
-    echo -n "Connecting to $friendly_name ($host:$port)"
+    local status
+
+    echo -n "Connecting to $server_name ($server_url)"
     while [ $now_s -le $stop_s ]; do
         echo -n .
-        $BIN/checksocket.sh $host $port >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo "OK (connected to $host:$port)"
+        status=`curl -o /dev/null -s -w %{http_code} $server_url`
+        if [ $status -eq 200 ]; then
+            echo "OK"
             return 0
         fi
         sleep $SLEEP_INTERVAL_S
         now_s=`date '+%s'`
     done
 
-    echo "timeout exceeded ($timeout_s seconds): could not connect to $host:$port" >&2
+    echo "timeout exceeded ($timeout_s seconds): could not connect to $server_url" >&2
     return 1
 }
 
-echo "Forking HugeServer..."
+echo "Starting HugeServer..."
 if [ -n "$VERBOSE" ]; then
-    "$BIN"/huge-server.sh conf/huge-server.properties &
+    "$BIN"/huge-server.sh "$BIN"/../conf/huge-server.properties &
 else
-    "$BIN"/huge-server.sh conf/huge-server.properties >/dev/null 2>&1 &
+    "$BIN"/huge-server.sh "$BIN"/../conf/huge-server.properties >/dev/null 2>&1 &
 fi
-wait_for_startup 'HugeServer' $HSRV_IP $HSRV_PORT $HSRV_STARTUP_TIMEOUT_S || {
+wait_for_startup 'HugeServer' "$HSRV_URL/graphs" $HSRV_STARTUP_TIMEOUT_S || {
     echo "See $BIN/../logs/huge-server.log for HugeServer log output."  >&2
 }
 disown
--- hugegraph-dist/src/assembly/static/bin/start-gremlinserver.sh
+++ hugegraph-dist/src/assembly/static/bin/start-gremlinserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-abs_path() {
+function abs_path() {
     SOURCE="${BASH_SOURCE[0]}"
     while [ -h "$SOURCE" ]; do
         DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -10,34 +10,52 @@ abs_path() {
     echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 }
 
-BIN=`abs_path`
-GREMLINSERVER_CONFIG_TAG=gremlin-server
-: ${GSRV_STARTUP_TIMEOUT_S:=20}
-: ${GSRV_SHUTDOWN_TIMEOUT_S:=20}
-# 这里的IP和prot应该要动态替换
-: ${GSRV_IP:=127.0.0.1}
-: ${GSRV_PORT:=8182}
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
 
-: ${SLEEP_INTERVAL_S:=2}
+BIN=`abs_path`
+
+eval $(parse_yaml "$BIN/../conf/gremlin-server.yaml")
+
+GSRV_HOST=$host
+GSRV_PORT=$port
+
+GSRV_STARTUP_TIMEOUT_S=20
+GSRV_SHUTDOWN_TIMEOUT_S=20
+SLEEP_INTERVAL_S=2
+
 VERBOSE=
 
 # wait_for_startup friendly_name host port timeout_s
-wait_for_startup() {
-    local friendly_name="$1"
+function wait_for_startup() {
+
+    local server_name="$1"
     local host="$2"
     local port="$3"
     local timeout_s="$4"
 
     local now_s=`date '+%s'`
     local stop_s=$(( $now_s + $timeout_s ))
-    local status=
 
-    echo -n "Connecting to $friendly_name ($host:$port)"
+    echo -n "Connecting to $server_name ($host:$port)"
     while [ $now_s -le $stop_s ]; do
         echo -n .
         $BIN/checksocket.sh $host $port >/dev/null 2>&1
         if [ $? -eq 0 ]; then
-            echo "OK (connected to $host:$port)"
+            echo "OK"
             return 0
         fi
         sleep $SLEEP_INTERVAL_S
@@ -48,12 +66,12 @@ wait_for_startup() {
     return 1
 }
 
-echo "Forking GremlinServer..."
+echo "Starting GremlinServer..."
 if [ -n "$VERBOSE" ]; then
-    "$BIN"/gremlin-server.sh conf/gremlin-server.yaml &
+    "$BIN"/gremlin-server.sh "$BIN"/../conf/gremlin-server.yaml &
 else
-    "$BIN"/gremlin-server.sh conf/gremlin-server.yaml >/dev/null 2>&1 &
+    "$BIN"/gremlin-server.sh "$BIN"/../conf/gremlin-server.yaml >/dev/null 2>&1 &
 fi
-wait_for_startup 'GremlinServer' $GSRV_IP $GSRV_PORT $GSRV_STARTUP_TIMEOUT_S || {
+wait_for_startup 'GremlinServer' $GSRV_HOST $GSRV_PORT $GSRV_STARTUP_TIMEOUT_S || {
     echo "See $BIN/../logs/gremlin-server.log for GremlinServer log output."  >&2
 }
