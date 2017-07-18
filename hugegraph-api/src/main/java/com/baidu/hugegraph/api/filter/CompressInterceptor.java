41cb8a646cc0689e3898c51b596340d08e1f7a77
Zhangmei Li <lizhangmei@baidu.com>
2017-07-18 19:57:07 +0800

    HugeGraph-463: fix compress error for gremlin api if gremlin server not start
    Change-Id: Ib4be80346e1d2c47bfa2bd5cc8ed89ca9991969e

--- hugegraph-api/src/main/java/com/baidu/hugegraph/api/filter/CompressInterceptor.java
+++ hugegraph-api/src/main/java/com/baidu/hugegraph/api/filter/CompressInterceptor.java
@@ -15,6 +15,11 @@ import javax.ws.rs.ext.Provider;
 import javax.ws.rs.ext.WriterInterceptor;
 import javax.ws.rs.ext.WriterInterceptorContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.baidu.hugegraph.server.HugeServer;
+
 @Provider
 @Singleton
 @CompressInterceptor.Compress
@@ -22,12 +27,34 @@ public class CompressInterceptor implements WriterInterceptor {
 
     public static final String GZIP = "gzip";
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(HugeServer.class);
+
     // Set compress output buffer size to 4KB (about 40~600 vertices)
     public static final int BUFFER_SIZE = 1024 * 4;
 
     @Override
     public void aroundWriteTo(WriterInterceptorContext context)
                               throws IOException, WebApplicationException {
+        // If there is no annotation(like exception), we don't compress it
+        if (context.getAnnotations().length > 0) {
+            try {
+                this.compress(context);
+            } catch (Throwable e) {
+                logger.warn("Failed to compress response", e);
+                /*
+                 * FIXME: This will cause java.lang.IllegalStateException:
+                 *  Illegal attempt to call getOutputStream() after getWriter()
+                 */
+                throw e;
+            }
+        }
+
+        context.proceed();
+    }
+
+    private void compress(WriterInterceptorContext context)
+                          throws IOException {
         // Get compress info from the @Compress annotation
         final Compress compression = getCompressAnnotation(context);
         final String encoding = compression.value();
@@ -43,16 +70,10 @@ public class CompressInterceptor implements WriterInterceptor {
         if (encoding.equalsIgnoreCase(GZIP)) {
             output = new GZIPOutputStream(context.getOutputStream(), buffer);
         } else {
-            /*
-             * NOTE: Currently we just support GZIP.
-             * FIXME: This will cause java.lang.IllegalStateException:
-             *   Illegal attempt to call getOutputStream() after getWriter()
-             */
+            // NOTE: Currently we just support GZIP.
             throw new WebApplicationException("Can't support: " + encoding);
         }
         context.setOutputStream(output);
-
-        context.proceed();
     }
 
     private static Compress getCompressAnnotation(WriterInterceptorContext c) {
