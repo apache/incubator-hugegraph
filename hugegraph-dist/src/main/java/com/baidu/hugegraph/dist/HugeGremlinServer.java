a8f71466387d81d27f9ea8f2296b499cf530126b
Liu Jie <liujie23@baidu.com>
2017-07-24 13:42:14 +0800

    HugeGraph-504: HugeGremlinServer, trivial fix with grammer adjudement and code style
    Change-Id: I805bdccf8fdd7612404396d305d18154cb240f56
    Signed-off-by: Liu Jie <liujie23@baidu.com>

--- hugegraph-dist/src/main/java/com/baidu/hugegraph/dist/HugeGremlinServer.java
+++ hugegraph-dist/src/main/java/com/baidu/hugegraph/dist/HugeGremlinServer.java
@@ -1,7 +1,25 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.baidu.hugegraph.dist;
 
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph
-        .GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.server.GraphManager;
 import org.apache.tinkerpop.gremlin.server.GremlinServer;
 import org.apache.tinkerpop.gremlin.server.Settings;
@@ -10,9 +28,6 @@ import org.slf4j.LoggerFactory;
 
 import com.baidu.hugegraph.HugeException;
 
-/**
- * Created by liningrui on 2017/5/10.
- */
 public class HugeGremlinServer {
 
     private static final Logger logger =
@@ -36,18 +51,16 @@ public class HugeGremlinServer {
     private static void startWithInjectTraversal(String[] args)
                                                  throws Exception {
         logger.info(GremlinServer.getHeader());
-        final String file;
-        if (args.length > 0)
-            file = args[0];
-        else
-            file = "conf/gremlin-server.yaml";
 
+        final String file;
         final Settings settings;
+
+        file = args.length > 0 ? args[0] : "conf/gremlin-server.yaml";
         try {
             settings = Settings.read(file);
         } catch (Exception ex) {
-            logger.error("Configuration file at {} could not be found or " +
-                         "parsed properly. [{}]", file, ex.getMessage());
+            logger.error("Can't found the configuration file at {} or " +
+                         "being parsed properly. [{}]", file, ex.getMessage());
             return;
         }
 
@@ -58,8 +71,8 @@ public class HugeGremlinServer {
         injectTraversalSource(server);
 
         server.start().exceptionally(t -> {
-            logger.error("Gremlin Server was unable to start and will now " +
-                         "begin shutdown: {}", t.getMessage());
+            logger.error("Gremlin Server was unable to start and will " +
+                         "shutdown now: {}", t.getMessage());
             server.stop().join();
             return null;
         }).join();
@@ -73,8 +86,8 @@ public class HugeGremlinServer {
             String gName = G_PREFIX + graph;
             if (graphManager.getTraversalSource(gName) != null) {
                 throw new HugeException(
-                          "Found existed name '%s' in global bindings, " +
-                          "it may lead gremlin query error.", gName);
+                          "Found existing name '%s' in global bindings, " +
+                          "it may lead to gremlin query error.", gName);
             }
             // Add a traversal source for all graphs with customed rule.
             graphManager.putTraversalSource(gName, g);
