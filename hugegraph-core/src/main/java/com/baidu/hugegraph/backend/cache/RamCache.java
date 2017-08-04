7b177ce567e17a9b3a01bd908af359de3b5c0079
Zhangmei Li <lizhangmei@baidu.com>
2017-08-04 11:29:16 +0800

    HugeGraph-555: fix dead lock of cache(race condition of access/enqueue and dequeue)
    Change-Id: I71ab15babfc34e6f361d393cd517e4237e4fe785

--- hugegraph-core/src/main/java/com/baidu/hugegraph/backend/cache/RamCache.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/backend/cache/RamCache.java
@@ -39,17 +39,18 @@ public class RamCache implements Cache {
 
     private static final Logger logger = LoggerFactory.getLogger(Cache.class);
 
-    private final KeyLock keyLock = new KeyLock();
+    private volatile long hits = 0L;
+    private volatile long miss = 0L;
 
-    private long hits = 0;
-    private long miss = 0;
-    private int capacity = 0;
     // Default expire time(ms)
-    private long expire = 0;
+    private volatile long expire = 0L;
+
+    private final KeyLock keyLock;
+    private final int capacity;
 
     // Implement LRU cache
-    private Map<Id, LinkNode<Id, Object>> map;
-    private LinkedQueueNonBigLock<Id, Object> queue;
+    private final Map<Id, LinkNode<Id, Object>> map;
+    private final LinkedQueueNonBigLock<Id, Object> queue;
 
     public RamCache() {
         this(DEFAULT_SIZE);
@@ -57,6 +58,8 @@ public class RamCache implements Cache {
 
     // NOTE: count in number of items, not in bytes
     public RamCache(int capacity) {
+        this.keyLock = new KeyLock();
+
         if (capacity < 8) {
             capacity = 8;
         }
@@ -71,16 +74,34 @@ public class RamCache implements Cache {
         this.queue = new LinkedQueueNonBigLock<>();
     }
 
-    private void access(LinkNode<Id, Object> node) {
-        // Ignore concurrent write for hits
-        ++this.hits;
+    private Object access(Id id) {
+        assert id != null;
 
-        Id id = node.key();
         this.keyLock.lock(id);
         try {
-            // Add to tail
-            this.queue.remove(node);
-            this.queue.enqueue(node);
+            LinkNode<Id, Object> node = this.map.get(id);
+            if (node == null) {
+                return null;
+            }
+
+            // Move the node from mid to tail
+            if (this.queue.remove(node) == null) {
+                // The node may be removed by others through dequeue()
+                return null;
+            }
+            /*
+             * FIXME: currently we use enqueue() with copy the node instead of
+             * enqueue(node) with the node itself due to the node may lead to
+             * dead lock. In the end we will use the sorted lock instead.
+             */
+            this.map.put(id, this.queue.enqueue(node.key(), node.value()));
+
+            // Ignore concurrent write for hits
+            logger.debug("RamCache cached '{}' (hits={}, miss={})",
+                         id, ++this.hits, this.miss);
+
+            assert id.equals(node.key());
+            return node.value();
         } finally {
             this.keyLock.unlock(id);
         }
@@ -88,24 +109,53 @@ public class RamCache implements Cache {
 
     private void write(Id id, Object value) {
         assert id != null;
+        assert this.capacity > 0;
 
         this.keyLock.lock(id);
         try {
-            if (this.map.size() < this.capacity) {
-                // Add the new item to tail, then map it
-                this.map.put(id, this.queue.enqueue(id, value));
-            } else {
-                // Remove the oldest
+            // The cache is full
+            while (this.map.size() >= this.capacity) {
+                /*
+                 * Remove the oldest from the queue
+                 * NOTE: it maybe return null if someone else (that's other
+                 * threads) are doing dequeue() and the queue may be empty.
+                 */
                 LinkNode<Id, Object> removed = this.queue.dequeue();
-                assert removed != null;
+                if (removed == null) {
+                    /*
+                     * If at this time someone add some new items, these will
+                     * be cleared in the map, but still stay in the queue, so
+                     * the queue will have some more nodes than the map.
+                     */
+                    this.map.clear();
+                    break;
+                }
+                /*
+                 * Remove the oldest from the map
+                 * NOTE: it maybe return null if other threads are doing remove
+                 */
                 this.map.remove(removed.key());
-                logger.debug("RamCache replace '{}' with '{}' (capacity={})",
-                             removed.value(), id, this.capacity);
+                logger.debug("RamCache replaced '{}' with '{}' (capacity={})",
+                             removed.key(), id, this.capacity);
+                /*
+                 * Release the object
+                 * NOTE: we can't reuse the removed node due to someone else
+                 * may access the node (will do remove() -> enqueue())
+                 */
+                removed = null;
+            }
 
-                // Reuse the removed node
-                LinkNode<Id, Object> newest = removed;
-                newest.reset(id, value);
-                this.map.put(id, newest);
+            LinkNode<Id, Object> node = this.map.get(id);
+            if (node != null) {
+                /*
+                 * Update the old value in the node if the id exists
+                 * FIXME: update the node position in queue(should remove it?)
+                 */
+                node.value(value);
+            } else {
+                // Add the new item to tail of the queue, then map it
+                assert !this.map.containsKey(id);
+                this.map.put(id, this.queue.enqueue(id, value));
             }
         } finally {
             this.keyLock.unlock(id);
@@ -117,7 +167,14 @@ public class RamCache implements Cache {
 
         this.keyLock.lock(id);
         try {
-            this.queue.remove(this.map.remove(id));
+            /*
+             * Remove the id from map and queue
+             * NOTE: it maybe return null if other threads have removed the id
+             */
+            LinkNode<Id, Object> node = this.map.remove(id);
+            if (node != null) {
+                this.queue.remove(node);
+            }
         } finally {
             this.keyLock.unlock(id);
         }
@@ -125,39 +182,39 @@ public class RamCache implements Cache {
 
     @Override
     public Object get(Id id) {
-        LinkNode<Id, Object> node = this.map.get(id);
-        if (node != null) {
-            this.access(node);
-            logger.debug("RamCache cached '{}' (hits={}, miss={})",
-                         id, this.hits, this.miss);
-            return node.value();
-        } else {
+        Object value = null;
+        if (this.map.containsKey(id)) {
+            // Maybe the id removed by other threads and returned null value
+            value = this.access(id);
+        }
+        if (value == null) {
             logger.debug("RamCache missed '{}' (miss={}, hits={})",
                          id, ++this.miss, this.hits);
-            return null;
         }
+        return value;
     }
 
     @Override
     public Object getOrFetch(Id id, Function<Id, Object> fetcher) {
-        LinkNode<Id, Object> node = this.map.get(id);
-        if (node != null) {
-            this.access(node);
-            logger.debug("RamCache cached '{}' (hits={}, miss={})",
-                         id, this.hits, this.miss);
-            return node.value();
-        } else {
+        Object value = null;
+        if (this.map.containsKey(id)) {
+            // Maybe the id removed by other threads and returned null value
+            value = this.access(id);
+        }
+        if (value == null) {
             logger.debug("RamCache missed '{}' (miss={}, hits={})",
                          id, ++this.miss, this.hits);
-            Object value = fetcher.apply(id);
+            // Do fetch and update the cache
+            value = fetcher.apply(id);
             this.update(id, value);
-            return value;
         }
+        return value;
     }
 
     @Override
     public void update(Id id, Object value) {
-        if (id == null || value == null) {
+        if (id == null || value == null ||
+            this.capacity <= 0) {
             return;
         }
         this.write(id, value);
@@ -165,7 +222,9 @@ public class RamCache implements Cache {
 
     @Override
     public void updateIfAbsent(Id id, Object value) {
-        if (id == null || value == null || this.map.containsKey(id)) {
+        if (id == null || value == null ||
+            this.capacity <= 0 ||
+            this.map.containsKey(id)) {
             return;
         }
         this.write(id, value);
@@ -236,14 +295,21 @@ public class RamCache implements Cache {
 
     private static class LinkNode<K, V> {
 
-        private K key;
+        private final K key;
         private V value;
         private long time;
         private LinkNode<K, V> prev;
         private LinkNode<K, V> next;
 
         public LinkNode(K key, V value) {
-            this.reset(key, value);
+            this.time = now();
+            this.key = key;
+            this.value = value;
+            this.prev = this.next = null;
+        }
+
+        public void value(V value) {
+            this.value = value;
         }
 
         public final K key() {
@@ -256,13 +322,6 @@ public class RamCache implements Cache {
 
         public long time() {
             return this.time;
-        }
-
-        public void reset(K key, V value) {
-            this.time = now();
-            this.key = key;
-            this.value = value;
-            this.prev = this.next = null;
         }
 
         @Override
@@ -303,16 +362,72 @@ public class RamCache implements Cache {
             assert this.rear.prev == this.head;
         }
 
+        /**
+         * Dump keys of all nodes in this queue (just for debug)
+         */
+        private List<K> dumpKeys() {
+            List<K> keys = new LinkedList<>();
+            LinkNode<K, V> node = this.head.next;
+            while (node != this.rear && node != this.empty) {
+                assert node != null;
+                keys.add(node.key());
+                node = node.next;
+            }
+            return keys;
+        }
+
+        /**
+         * Check whether a key not in this queue (just for debug)
+         */
+        private boolean checkNotInQueue(K key) {
+            List<K> keys = this.dumpKeys();
+            if (keys.contains(key)) {
+                throw new RuntimeException(String.format(
+                          "Expect %s should be not in %s", key, keys));
+            }
+            return true;
+        }
+
+        /**
+         * Check whether there is circular reference (just for debug)
+         * NOTE: but it is important to note that this is only key check
+         * rather than pointer check.
+         */
+        private boolean checkPrevNotInNext(LinkNode<K, V> self) {
+            LinkNode<K, V> prev = self.prev;
+            if (prev.key() == null) {
+                assert prev == this.head || prev == this.empty : prev;
+                return true;
+            }
+            List<K> keys = this.dumpKeys();
+            int prevPos = keys.indexOf(prev.key());
+            int selfPos = keys.indexOf(self.key());
+            if (prevPos > selfPos && selfPos != -1) {
+                throw new RuntimeException(String.format(
+                          "Expect %s should be before %s, actual %s",
+                          prev.key(), self.key(), keys));
+            }
+            return true;
+        }
+
         public void clear() {
             synchronized (this.rear) {
-                /*
-                 * If someone is removing the last node by remove(),
-                 * it will update the rear.prev, so we should lock it.
-                 */
                 assert this.rear.prev != null : this.head.next;
-                synchronized (this.rear.prev) {
-                    synchronized (this.head) {
-                        this.reset();
+                while (true) {
+                    /*
+                     * If someone is removing the last node by remove(),
+                     * it will update the rear.prev, so we should lock it.
+                     */
+                    LinkNode<K, V> last = this.rear.prev;
+                    synchronized (last) {
+                        if (last != this.rear.prev) {
+                            // The rear.prev has changed, try to get lock again
+                            continue;
+                        }
+                        synchronized (this.head) {
+                            this.reset();
+                        }
+                        return;
                     }
                 }
             }
@@ -323,6 +438,8 @@ public class RamCache implements Cache {
         }
 
         public final LinkNode<K, V> enqueue(LinkNode<K, V> node) {
+            assert node != null;
+            // TODO: should we lock the new `node`?
             synchronized (this.rear) {
                 while (true) {
                     LinkNode<K, V> last = this.rear.prev;
@@ -331,13 +448,20 @@ public class RamCache implements Cache {
                             // The rear.prev has changed, try to get lock again
                             continue;
                         }
-                        // Build the link between `last` and `node`
-                        last.next = node;
-                        node.prev = last;
+                        assert this.checkNotInQueue(node.key());
+                        /*
+                         * Link the node to the rear before to the last if we
+                         * have not locked the node itself, because dumpKeys()
+                         * may get the new node with next=null.
+                         * TODO: it also depends on memory barrier.
+                         */
                         // Build the link between `node` and the rear
                         node.next = this.rear;
                         assert this.rear.prev == last : this.rear.prev;
                         this.rear.prev = node;
+                        // Build the link between `last` and `node`
+                        node.prev = last;
+                        last.next = node;
 
                         return node;
                     }
@@ -346,85 +470,73 @@ public class RamCache implements Cache {
         }
 
         public final LinkNode<K, V> dequeue() {
-            synchronized (this.head) {
-                /*
-                 * There is no need to lock this.head.next because the remove()
-                 * can't get its previous lock(that's the locked head here).
-                 */
-                boolean updatedTheOnlyOneElem = false;
-
-                LinkNode<K, V> node = this.head.next;
-                if (node == this.rear) {
-                    // Empty queue
-                    return null;
-                }
-
-                /*
-                 * If there is only one element, the rear.prev would point to
-                 * the deleting node, so we should lock the rear.prev to avoid
-                 * enqueue() which may update the rear.prev(that's `node`).
-                 */
-                if (node == this.rear.prev) {
-                    /*
-                     * FIXME: Maybe lead dead lock if someone else is removing
-                     * `node` or clearing the queue and catching the lock of
-                     * the rear.(but dequeue() would only be called when the
-                     * cache is full--in general one element is not full)
-                     */
-                    synchronized (node) {
-                        if (node == this.rear.prev) {
-                            updatedTheOnlyOneElem = true;
-                            // Break the link between the head and `node`
-                            this.head.next = node.next;
-                            node.next.prev = this.head;
-                        }
+            while (true) {
+                LinkNode<K, V> first = this.head.next;
+                synchronized (first) {
+                    if (first != this.head.next) {
+                        // The head.next has changed, try to get lock again
+                        continue;
                     }
-                }
+                    if (first == this.rear) {
+                        // Empty queue
+                        return null;
+                    }
+                    synchronized (this.head) {
+                        // Break the link between the head and `first`
+                        assert first.next != null;
+                        this.head.next = first.next;
+                        first.next.prev = this.head;
 
-                if (!updatedTheOnlyOneElem) {
-                    // Break the link between the head and `node`(without lock)
-                    this.head.next = node.next;
-                    node.next.prev = this.head;
+                        // Clear the links of the first node
+                        first.prev = this.empty;
+                        first.next = this.empty;
+                    }
+                    return first;
                 }
-                // Clear `node` links
-                node.prev = this.empty;
-                node.next = this.empty;
-
-                return node;
             }
         }
 
-        public final void remove(LinkNode<K, V> node) {
+        public final LinkNode<K, V> remove(LinkNode<K, V> node) {
             synchronized (node) {
                 assert node != this.empty;
                 assert node != this.head && node != this.rear;
-                if (node.next == node.prev && node.prev == this.empty) {
-                    // Ignore the node if it has been removed
-                    return;
-                }
+
                 while (true) {
                     LinkNode<K, V> prev = node.prev;
+                    if (prev == this.empty) {
+                        assert node.next == this.empty;
+                        // Ignore the node if it has been removed
+                        return null;
+                    }
+
+                    // We can use the assertion to debug circular reference:
+                    // assert this.checkPrevNotInNext(node);
+
                     synchronized (prev) {
                         if (prev != node.prev) {
                             /*
-                             * The previous node has changed(maybe it released
-                             * lock after it's removed, and we got the lock),
-                             * so try again until it's not changed.
+                             * The previous node has changed (maybe it's lock
+                             * released after it's removed, then we got the
+                             * lock), so try again until it's not changed.
                              */
                             continue;
                         }
+                        assert node.next != null : node;
                         assert node.next != node.prev : node.next;
 
                         // Build the link between node.prev and node.next
                         node.prev.next = node.next;
                         node.next.prev = node.prev;
 
-                        // Clear node links
-                        assert prev == node.prev;
+                        // Assert to debug the queue state
+                        assert this.checkPrevNotInNext(node.next);
+                        assert prev == node.prev : prev.key + "!=" + node.prev;
+
+                        // Clear the links of `node`
                         node.prev = this.empty;
                         node.next = this.empty;
 
-                        return;
+                        return node;
                     }
                 }
             }
