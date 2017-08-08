705c27b9860ac8965ded35c61596fcc21f88362e
Zhangmei Li <lizhangmei@baidu.com>
2017-08-08 14:53:16 +0800

    HugeGraph-555: add lockAll() for KeyLock
    Change-Id: Iac0a396077658b82b67547e3eea5793d54fa01fd

--- src/main/java/com/baidu/hugegraph/concurrent/KeyLock.java
+++ src/main/java/com/baidu/hugegraph/concurrent/KeyLock.java
@@ -19,10 +19,17 @@
 
 package com.baidu.hugegraph.concurrent;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.locks.Lock;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.Striped;
 
+/**
+ * KeyLock provide an interface of segment lock
+ */
 public class KeyLock {
 
     private Striped<Lock> locks;
@@ -36,11 +43,92 @@ public class KeyLock {
         this.locks = Striped.lock(size);
     }
 
-    public final void lock(Object key) {
-        this.locks.get(key).lock();
+    private int indexOf(Lock lock) {
+        for (int i = 0; i < this.locks.size(); i++) {
+            if (this.locks.getAt(i) == lock) {
+                return i;
+            }
+        }
+        return -1;
     }
 
+    /**
+     * Lock an object
+     * @param key The object to lock
+     * @return The lock(locked) of passed key
+     */
+    public final Lock lock(Object key) {
+        Lock lock = this.locks.get(key);
+        lock.lock();
+        return lock;
+    }
+
+    /**
+     * Unlock an object
+     * @param key The object to unlock
+     */
     public final void unlock(Object key) {
         this.locks.get(key).unlock();
+    }
+
+    /**
+     * Lock a list of object with sorted order
+     * @param keys The objects to lock
+     * @return The locks(locked) of keys
+     */
+    public final List<Lock> lockAll(Object... keys) {
+        List<Lock> locks = new ArrayList<>(keys.length);
+        for (Object key : keys) {
+            Lock lock = this.locks.get(key);
+            locks.add(lock);
+        }
+        Collections.sort(locks, (a, b) -> {
+            int diff = a.hashCode() - b.hashCode();
+            if (diff == 0 && a != b) {
+                diff = this.indexOf(a) - this.indexOf(b);
+                assert diff != 0;
+            }
+            return diff;
+        });
+        for (int i = 0; i < locks.size(); i++) {
+            locks.get(i).lock();
+        }
+        return Collections.unmodifiableList(locks);
+    }
+
+    /**
+     * Lock two objects with sorted order
+     * NOTE: This is to optimize the performance of lockAll(keys)
+     */
+    public List<Lock> lockAll(Object key1, Object key2) {
+        Lock lock1 = this.locks.get(key1);
+        Lock lock2 = this.locks.get(key2);
+
+        int diff = lock1.hashCode() - lock2.hashCode();
+        if (diff == 0 && lock1 != lock2) {
+            diff = this.indexOf(lock1) - this.indexOf(lock2);
+            assert diff != 0;
+        }
+
+        List<Lock> locks = diff > 0 ?
+                           ImmutableList.of(lock2, lock1) :
+                           ImmutableList.of(lock1, lock2);
+
+        for (int i = 0; i < locks.size(); i++) {
+            locks.get(i).lock();
+        }
+
+        return locks;
+    }
+
+    /**
+     * Unlock a list of object
+     * @param locks The locks to unlock
+     */
+    public final void unlockAll(List<Lock> locks) {
+        for (int i = locks.size(); i > 0; i--) {
+            assert this.indexOf(locks.get(i - 1)) != -1;
+            locks.get(i - 1).unlock();
+        }
     }
 }
