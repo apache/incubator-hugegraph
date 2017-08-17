db832466996bf2670aa6526bdcc2992e72e2e72d
liningrui <liningrui@baidu.com>
2017-08-17 16:26:01 +0800

    HugeGraph-645: Add empty line between license and package for hugegraph-server
    Change-Id: I301b736be05cfb80524b004ed4af332d14262a68

--- hugegraph-cassandra/src/main/java/com/baidu/hugegraph/backend/store/cassandra/CassandraStore.java
+++ hugegraph-cassandra/src/main/java/com/baidu/hugegraph/backend/store/cassandra/CassandraStore.java
@@ -16,6 +16,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+
 package com.baidu.hugegraph.backend.store.cassandra;
 
 import java.util.List;
--- hugegraph-test/src/main/java/com/baidu/hugegraph/api/EdgeApiTest.java
+++ hugegraph-test/src/main/java/com/baidu/hugegraph/api/EdgeApiTest.java
@@ -16,6 +16,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+
 package com.baidu.hugegraph.api;
 
 import javax.ws.rs.core.Response;
