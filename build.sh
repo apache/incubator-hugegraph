8a44989e32959c0a8b607c3a5524d4279e0e3381
liningrui <liningrui@baidu.com>
2017-12-19 20:19:32 +0800

    HugeGraph-990: Fixed bug that compile error when building project
    Change-Id: I398165b3b022e55290781b366e2d008f516dfb46

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-HUGEGRAPH_COMMON_RELEASE_PATH="${PWD}/output/"
-
-export MAVEN_HOME="/home/scmtools/buildkit/maven/apache-maven-3.3.9/"
-export JAVA_HOME="/home/scmtools/buildkit/java/jdk1.8.0_25/"
-export PATH="$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH"
+export MAVEN_HOME=/home/scmtools/buildkit/maven/apache-maven-3.3.9/
+export JAVA_HOME=/home/scmtools/buildkit/java/jdk1.8.0_25/
+export PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH
 
 mvn clean compile
