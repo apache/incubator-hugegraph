a04362407e90266e8e8075c11837e60b57b1ec62
zhangyi51 <zhangyi51@baidu.com>
2018-02-28 19:17:06 +0800

    hugegraph-1150 retry initBackend if timeout or keysapce/table not exist
    Change-Id: I5d170cc14888b6c3f0aef659c780f5b900ecd5d8

--- hugegraph-dist/src/main/java/com/baidu/hugegraph/dist/InitStore.java
+++ hugegraph-dist/src/main/java/com/baidu/hugegraph/dist/InitStore.java
@@ -19,25 +19,45 @@
 
 package com.baidu.hugegraph.dist;
 
+import java.util.Collection;
 import java.util.List;
 
+import org.apache.commons.collections.map.MultiValueMap;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.tree.ConfigurationNode;
 import org.apache.tinkerpop.gremlin.util.config.YamlConfiguration;
+import org.slf4j.Logger;
 
 import com.baidu.hugegraph.HugeFactory;
 import com.baidu.hugegraph.HugeGraph;
 import com.baidu.hugegraph.config.ServerOptions;
 import com.baidu.hugegraph.event.EventHub;
 import com.baidu.hugegraph.util.E;
+import com.baidu.hugegraph.util.Log;
 
 public class InitStore {
 
+    private static final Logger LOG = Log.logger(InitStore.class);
+
     private static final String GRAPHS = ServerOptions.GRAPHS.name();
+    // 6~8 retries may be needed under high load for Cassandra backend
+    private static final int RETRIES = 10;
+    // Less than 5000 may cause mismatch exception with Cassandra backend
+    private static final long RETRY_INTERVAL = 5000;
+
+    private static MultiValueMap exceptions = new MultiValueMap();
+
+    static {
+        exceptions.put("OperationTimedOutException",
+                       "Timed out waiting for server response");
+        exceptions.put("NoHostAvailableException",
+                       "All host(s) tried for query failed");
+        exceptions.put("InvalidQueryException", "does not exist");
+        exceptions.put("InvalidQueryException", "unconfigured table");
+    }
 
     public static void main(String[] args)
-                  throws ConfigurationException, InterruptedException {
-
+                       throws ConfigurationException, InterruptedException {
         E.checkArgument(args.length == 1,
                         "Init store only accept one config file.");
         E.checkArgument(args[0].endsWith(".yaml"),
@@ -61,15 +81,44 @@ public class InitStore {
                         "Must contain at least one graph");
 
         for (ConfigurationNode graphName : graphNames) {
-            String graphPropFile = graphName.getValue().toString();
-            // Get graph property file path
-            HugeGraph graph = HugeFactory.open(graphPropFile);
-            graph.initBackend();
-            graph.close();
+            String configPath = graphName.getValue().toString();
+            initGraph(configPath);
         }
 
         // Wait cache clear or init up to 30 seconds
         EventHub.destroy(30);
     }
 
+    private static void initGraph(String config) throws InterruptedException {
+        LOG.info("Init graph with config file: {}", config);
+        HugeGraph graph = HugeFactory.open(config);
+
+        int retries = RETRIES;
+        retry: do {
+            try {
+                graph.initBackend();
+            } catch (Exception e) {
+                String clz = e.getClass().getSimpleName();
+                String message = e.getMessage();
+                if (exceptions.containsKey(clz) && retries > 0) {
+                    @SuppressWarnings("unchecked")
+                    Collection<String> keywords = exceptions.getCollection(clz);
+                    for (String keyword : keywords) {
+                        if (message.contains(keyword)) {
+                            LOG.info("Init failed with exception '{} : {}', " +
+                                     "retry  {}...",
+                                     clz, message, RETRIES - retries + 1);
+
+                            Thread.sleep(RETRY_INTERVAL);
+                            continue retry;
+                        }
+                    }
+                }
+                throw e;
+            }
+            break;
+        } while(retries-- > 0);
+
+        graph.close();
+    }
 }
