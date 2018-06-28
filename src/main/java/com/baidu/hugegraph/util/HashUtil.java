92ad62a9fbaad7f194255bf1ffff96bceb3b1de3
Zhangmei Li <lizhangmei@baidu.com>
2018-06-28 16:20:54 +0800

    HugeGraph-1330: add support for 128bits hash
    Change-Id: I51b46d3a88e472757e4ca45e73e7d6011150ef2f

--- src/main/java/com/baidu/hugegraph/util/HashUtil.java
+++ src/main/java/com/baidu/hugegraph/util/HashUtil.java
@@ -21,17 +21,26 @@ package com.baidu.hugegraph.util;
 
 import java.nio.charset.Charset;
 
+import com.google.common.base.Charsets;
 import com.google.common.hash.Hashing;
 
 public final class HashUtil {
+
+    private static final Charset CHARSET = Charsets.UTF_8;
 
     public static byte[] hash(byte[] bytes) {
         return Hashing.murmur3_32().hashBytes(bytes).asBytes();
     }
 
     public static String hash(String value) {
-        final Charset charset = Charset.forName("UTF-8");
-        return Hashing.murmur3_32().hashString(value, charset).toString();
+        return Hashing.murmur3_32().hashString(value, CHARSET).toString();
     }
 
+    public static byte[] hash128(byte[] bytes) {
+        return Hashing.murmur3_128().hashBytes(bytes).asBytes();
+    }
+
+    public static String hash128(String value) {
+        return Hashing.murmur3_128().hashString(value, CHARSET).toString();
+    }
 }
