1789a134e73703f368dd88786608b8c423ff48d0
liningrui <liningrui@baidu.com>
2018-07-17 15:47:40 +0800

    HugeGraph-1342: Arrange the util.sh in hugegraph-dist and hugegraph-tools module
    Change-Id: Id16697f75bf447e91a0ace70720067962528102e

--- hugegraph-dist/src/assembly/static/bin/util.sh
+++ hugegraph-dist/src/assembly/static/bin/util.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
-#read a property from a .properties file
-function read_property(){
+# read a property from .properties file
+function read_property() {
+    # file path
+    file_name=$1
     # replace "." to "\."
     property_name=`echo $2 | sed 's/\./\\\./g'`
-    # file path
-    file_name=$1;
     cat $file_name | sed -n -e "s/^[ ]*//g;/^#/d;s/^$property_name=//p" | tail -1
+}
+
+function write_property() {
+    local file=$1
+    local key=$2
+    local value=$3
+
+    local os=`uname`
+    case $os in
+        # Note: in mac os should use sed -i '' "xxx" to replace string,
+        # otherwise prompt 'command c expects \ followed by text'.
+        # See http://www.cnblogs.com/greedy-day/p/5952899.html
+        Darwin) sed -i '' "s!$key=.*!$key=$value!g" "$file" ;;
+        *) sed -i "s!$key=.*!$key=$value!g" "$file" ;;
+    esac
+}
+
+function parse_yaml() {
+    local file=$1
+    local version=$2
+    local module=$3
+
+    cat $file | tr -d '\n {}'| awk -F',+|:' '''{
+        pre="";
+        for(i=1; i<=NF; ) {
+            if(match($i, /version/)) {
+                pre=$i;
+                i+=1
+            } else {
+                result[pre"-"$i] = $(i+1);
+                i+=2
+            }
+        }
+    } END {for(e in result) {print e": "result[e]}}''' \
+    | grep "$version-$module" | awk -F':' '{print $2}' | tr -d ' ' && echo
 }
 
 function process_num() {
@@ -85,67 +120,6 @@ function wait_for_startup() {
     return 1
 }
 
-wait_for_shutdown() {
-    local process_name="$1"
-    local pid="$2"
-    local timeout_s="$3"
-
-    local now_s=`date '+%s'`
-    local stop_s=$(( $now_s + $timeout_s ))
-
-    echo -n "Killing $process_name(pid $pid)" >&2
-    while [ $now_s -le $stop_s ]; do
-        echo -n .
-        process_status "$process_name" "$pid" >/dev/null
-        if [ $? -eq 1 ]; then
-            echo "OK"
-            return 0
-        fi
-        sleep 2
-        now_s=`date '+%s'`
-    done
-    echo "$process_name shutdown timeout(exceeded $timeout_s seconds)" >&2
-    return 1
-}
-
-process_status() {
-    local process_name="$1"
-    local pid="$2"
-
-    ps -p "$pid"
-    if [ $? -eq 0 ]; then
-        echo "$process_name is running with pid $pid"
-        return 0
-    else
-        echo "The process $process_name does not exist"
-        return 1
-    fi
-}
-
-kill_process() {
-    local process_name="$1"
-    local pid="$2"
-
-    if [ -z "$pid" ]; then
-        echo "The process $pid does not exist"
-        return 0
-    fi
-
-    case "`uname`" in
-        CYGWIN*) taskkill /F /PID "$pid" ;;
-        *)       kill "$pid" ;;
-    esac
-}
-
-function kill_process_and_wait() {
-    local process_name="$1"
-    local pid="$2"
-    local timeout_s="$3"
-
-    kill_process "$process_name" "$pid"
-    wait_for_shutdown "$process_name" "$pid" "$timeout_s"
-}
-
 function free_memory() {
     local free=""
     local os=`uname`
@@ -186,4 +160,152 @@ function calc_xmx() {
         xmx=$half_free
     fi
     echo $xmx
+}
+
+function remove_with_prompt() {
+    local path=$1
+    local tips=""
+
+    if [ -d "$path" ]; then
+        tips="Remove directory '$path' and all sub files [y/n]?"
+    elif [ -f "$path" ]; then
+        tips="Remove file '$path' [y/n]?"
+    else
+        return 0
+    fi
+
+    read -p "$tips " yn
+    case $yn in
+        [Yy]* ) rm -rf "$path";;
+        * ) ;;
+    esac
+}
+
+function ensure_path_writable() {
+    local path=$1
+    # Ensure input path exist
+    if [ ! -d "${path}" ]; then
+        mkdir -p ${path}
+    fi
+    # Check for write permission
+    if [ ! -w "${path}" ]; then
+        echo "No write permission on directory ${path}"
+        exit 1
+    fi
+}
+
+function get_ip() {
+    local os=`uname`
+    local loopback="127.0.0.1"
+    local ip=""
+    case $os in
+        Linux) ip=`ifconfig | grep 'inet addr:'| grep -v "$loopback" | cut -d: -f2 | awk '{ print $1}'`;;
+        FreeBSD|OpenBSD|Darwin) ip=`ifconfig  | grep -E 'inet.[0-9]' | grep -v "$loopback" | awk '{ print $2}'`;;
+        SunOS) ip=`ifconfig -a | grep inet | grep -v "$loopback" | awk '{ print $2} '`;;
+        *) ip=$loopback;;
+    esac
+    echo $ip
+}
+
+function download() {
+    local path=$1
+    local link_url=$2
+
+    if command -v wget >/dev/null 2>&1; then
+        wget --help | grep -q '\--show-progress' && progress_opt="-q --show-progress" || progress_opt=""
+        wget ${link_url} -P ${path} $progress_opt
+    elif command -v curl >/dev/null 2>&1; then
+        curl ${link_url} -o ${path}/${link_url}
+    else
+        echo "Required wget or curl but they are not installed"
+    fi
+}
+
+function ensure_package_exist() {
+    local path=$1
+    local dir=$2
+    local tar=$3
+    local link=$4
+
+    if [ ! -d ${path}/${dir} ]; then
+        if [ ! -f ${path}/${tar} ]; then
+            echo "Downloading the compressed package '${tar}'"
+            download ${path} ${link}
+            if [ $? -ne 0 ]; then
+                echo "Failed to download, please ensure the network is available and link is valid"
+                exit 1
+            fi
+            echo "[OK] Finished download"
+        fi
+        echo "Unzip the compressed package '$tar'"
+        tar -zxvf ${path}/${tar} -C ${path} >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "Failed to unzip, please check the compressed package"
+            exit 1
+        fi
+        echo "[OK] Finished unzip"
+    fi
+}
+
+###########################################################################
+
+function wait_for_shutdown() {
+    local process_name="$1"
+    local pid="$2"
+    local timeout_s="$3"
+
+    local now_s=`date '+%s'`
+    local stop_s=$(( $now_s + $timeout_s ))
+
+    echo -n "Killing $process_name(pid $pid)" >&2
+    while [ $now_s -le $stop_s ]; do
+        echo -n .
+        process_status "$process_name" "$pid" >/dev/null
+        if [ $? -eq 1 ]; then
+            echo "OK"
+            return 0
+        fi
+        sleep 2
+        now_s=`date '+%s'`
+    done
+    echo "$process_name shutdown timeout(exceeded $timeout_s seconds)" >&2
+    return 1
+}
+
+function process_status() {
+    local process_name="$1"
+    local pid="$2"
+
+    ps -p "$pid"
+    if [ $? -eq 0 ]; then
+        echo "$process_name is running with pid $pid"
+        return 0
+    else
+        echo "The process $process_name does not exist"
+        return 1
+    fi
+}
+
+function kill_process() {
+    local process_name="$1"
+    local pid="$2"
+
+    if [ -z "$pid" ]; then
+        echo "The process $pid does not exist"
+        return 0
+    fi
+
+    case "`uname`" in
+        CYGWIN*) taskkill /F /PID "$pid" ;;
+        *)       kill "$pid" ;;
+    esac
+}
+
+function kill_process_and_wait() {
+    local process_name="$1"
+    local pid="$2"
+    local timeout_s="$3"
+
+    kill_process "$process_name" "$pid"
+    wait_for_shutdown "$process_name" "$pid" "$timeout_s"
 }
