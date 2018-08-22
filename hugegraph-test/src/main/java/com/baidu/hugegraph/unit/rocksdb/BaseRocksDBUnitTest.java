c041992cd80734a4b779f6c3610674265daa999c
zhoney <zhangyi51@baidu>
2018-08-22 19:32:39 +0800

    fix bug init-store failed due to datapath and walpath error for rocksdb backend
    fix #22
    
    Change-Id: Id86628f0073301a969d054d4e52b115ac9096c14

--- hugegraph-test/src/main/java/com/baidu/hugegraph/unit/rocksdb/BaseRocksDBUnitTest.java
+++ hugegraph-test/src/main/java/com/baidu/hugegraph/unit/rocksdb/BaseRocksDBUnitTest.java
@@ -34,7 +34,6 @@ import org.junit.Before;
 import org.mockito.Mockito;
 import org.rocksdb.RocksDBException;
 
-import com.baidu.hugegraph.backend.store.rocksdb.RocksDBOptions;
 import com.baidu.hugegraph.backend.store.rocksdb.RocksDBSessions;
 import com.baidu.hugegraph.backend.store.rocksdb.RocksDBStdSessions;
 import com.baidu.hugegraph.config.HugeConfig;
@@ -117,9 +116,8 @@ public class BaseRocksDBUnitTest extends BaseUnitTest {
         Configuration conf = Mockito.mock(PropertiesConfiguration.class);
         Mockito.when(conf.getKeys()).thenReturn(Collections.emptyIterator());
         HugeConfig config = new HugeConfig(conf);
-        config.setProperty(RocksDBOptions.DATA_PATH.name(), DB_PATH);
-        config.setProperty(RocksDBOptions.WAL_PATH.name(), DB_PATH);
-        RocksDBSessions rocks = new RocksDBStdSessions(config, "db", "store");
+        RocksDBSessions rocks = new RocksDBStdSessions(config, DB_PATH, DB_PATH,
+                                                       "db", "store");
         rocks.createTable(table);
         return rocks;
     }
