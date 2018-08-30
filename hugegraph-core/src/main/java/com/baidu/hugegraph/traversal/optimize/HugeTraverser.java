a4561bfbe58bc6fc133f1c0da23d2e97ce008c62
zhoney <zhangyi51@baidu>
2018-08-30 19:52:32 +0800

    add rays and rings restful api
    fix #39
    
    Change-Id: I41f82b73cee0ce657b3bcaaf1a68cf3479788fbf

--- hugegraph-core/src/main/java/com/baidu/hugegraph/traversal/optimize/HugeTraverser.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/traversal/optimize/HugeTraverser.java
@@ -138,6 +138,45 @@ public class HugeTraverser {
         return paths;
     }
 
+    public List<Path> rays(Id sourceV, Directions dir, String label,
+                           int maxDepth, long degree, long capacity,
+                           long limit) {
+        return this.subGraphPaths(sourceV, dir, label, maxDepth, degree,
+                                  capacity, limit, false);
+    }
+
+    public List<Path> rings(Id sourceV, Directions dir, String label,
+                            int maxDepth, long degree, long capacity,
+                            long limit) {
+        return this.subGraphPaths(sourceV, dir, label, maxDepth, degree,
+                                  capacity, limit, true);
+    }
+
+    private List<Path> subGraphPaths(Id sourceV, Directions dir, String label,
+                                     int maxDepth, long degree, long capacity,
+                                     long limit, boolean rings) {
+        E.checkNotNull(sourceV, "source vertex id");
+        E.checkNotNull(dir, "direction");
+        checkPositive(maxDepth, "Paths max depth");
+        checkDegree(degree);
+        checkCapacity(capacity);
+        checkLimit(limit);
+
+        Id labelId = this.getEdgeLabelId(label);
+        SubGraphTraverser traverser = new SubGraphTraverser(sourceV, labelId,
+                                                            degree, capacity,
+                                                            limit, rings);
+        List<Path> paths = new ArrayList<>();
+        while (true) {
+            paths.addAll(traverser.forward(dir));
+            if (--maxDepth < 0 || traverser.reachLimit() ||
+                traverser.finished()) {
+                break;
+            }
+        }
+        return paths;
+    }
+
     public Set<Id> kout(Id sourceV, Directions dir, String label,
                         int depth, boolean nearest,
                         long degree, long capacity, long limit) {
@@ -408,7 +447,7 @@ public class HugeTraverser {
             return PATH_NONE;
         }
 
-        public boolean reachCapacity() {
+        private boolean reachCapacity() {
             if (this.capacity == NO_LIMIT || this.size < this.capacity) {
                 return false;
             }
@@ -539,11 +578,11 @@ public class HugeTraverser {
             return paths;
         }
 
-        public int accessedNodes() {
+        private int accessedNodes() {
             return this.sourcesAll.size() + this.targetsAll.size();
         }
 
-        public boolean reachLimit() {
+        private boolean reachLimit() {
             if (this.capacity != NO_LIMIT &&
                 this.accessedNodes() > this.capacity) {
                 return true;
@@ -552,6 +591,101 @@ public class HugeTraverser {
                 return false;
             }
             return true;
+        }
+    }
+
+    private class SubGraphTraverser {
+
+        private MultivaluedMap<Id, Node> sources = newMultivalueMap();
+        private Set<Id> accessedVertices = newSet();
+
+        private final Id label;
+        private final long degree;
+        private final long capacity;
+        private final long limit;
+        private final boolean rings;
+        private long pathCount;
+
+        public SubGraphTraverser(Id sourceV, Id label, long degree,
+                                 long capacity, long limit, boolean rings) {
+            this.sources.add(sourceV, new Node(sourceV));
+            this.accessedVertices.add(sourceV);
+            this.label = label;
+            this.degree = degree;
+            this.capacity = capacity;
+            this.limit = limit;
+            this.rings = rings;
+            this.pathCount = 0L;
+        }
+
+        /**
+         * Search forward from source
+         */
+        public List<Path> forward(Directions direction) {
+            List<Path> paths = new ArrayList<>();
+            MultivaluedMap<Id, Node> newVertices = newMultivalueMap();
+            Iterator<Edge> edges;
+            // Traversal vertices of previous level
+            for (List<Node> nodes : this.sources.values()) {
+                for (Node n : nodes) {
+                    edges = edgesOfVertex(n.id(), direction,
+                                          this.label, this.degree);
+                    if (!edges.hasNext()) {
+                        // Reach the end, rays found
+                        if (this.rings) {
+                            continue;
+                        }
+                        // Store rays
+                        paths.add(new Path(null, n.path()));
+                        this.pathCount++;
+                        if (reachLimit()) {
+                            return paths;
+                        }
+                        continue;
+                    }
+                    while (edges.hasNext()) {
+                        HugeEdge edge = (HugeEdge) edges.next();
+                        Id target = edge.id().otherVertexId();
+                        this.accessedVertices.add(target);
+
+                        if (!n.contains(target)) {
+                            // Add node to next start-nodes
+                            newVertices.add(target, new Node(target, n));
+                            continue;
+                        }
+                        // Rings found and expect rings
+                        if (this.rings) {
+                            assert n.contains(target);
+                            List<Id> prePath = n.path();
+                            prePath.add(target);
+                            paths.add(new Path(null, prePath));
+                            this.pathCount++;
+                            if (reachLimit()) {
+                                return paths;
+                            }
+                        }
+                    }
+                }
+            }
+            // Re-init sources
+            this.sources = newVertices;
+
+            return paths;
+        }
+
+        private boolean reachLimit() {
+            if (this.capacity != NO_LIMIT &&
+                this.accessedVertices.size() > this.capacity) {
+                return true;
+            }
+            if (this.limit == NO_LIMIT || this.pathCount < this.limit) {
+                return false;
+            }
+            return true;
+        }
+
+        private boolean finished() {
+            return this.sources.isEmpty();
         }
     }
 
