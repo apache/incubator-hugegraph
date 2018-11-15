mengquanrun/hugegraph

Support metrics of backend store
Implement #136

Change-Id: I3524bd483f33e57a8b0c0a742861c829346f0ec6

hugegraph-core/src/main/java/com/baidu/hugegraph/backend/store/memory/InMemoryDBStore.java
@@ -30,12 +30,14 @@ import com.baidu.hugegraph.backend.LocalCounter;
 import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.backend.query.Query;
 import com.baidu.hugegraph.backend.serializer.TextBackendEntry;
+import com.baidu.hugegraph.backend.store.AbstractBackendStore;
 import com.baidu.hugegraph.backend.store.BackendAction;
 import com.baidu.hugegraph.backend.store.BackendEntry;
 import com.baidu.hugegraph.backend.store.BackendFeatures;
 import com.baidu.hugegraph.backend.store.BackendMutation;
-import com.baidu.hugegraph.backend.store.BackendStore;
+import com.baidu.hugegraph.backend.store.BackendSession;
 import com.baidu.hugegraph.backend.store.BackendStoreProvider;
+import com.baidu.hugegraph.backend.store.MetaDispatcher;
 import com.baidu.hugegraph.config.HugeConfig;
 import com.baidu.hugegraph.type.HugeType;
 import com.baidu.hugegraph.util.Log;
@@ -53,7 +55,7 @@ import com.baidu.hugegraph.util.Log;
  * 1.remove by id + condition
  * 2.append/subtract edge-property
  */
-public class InMemoryDBStore implements BackendStore {
+public class InMemoryDBStore extends AbstractBackendStore {
 
     private static final Logger LOG = Log.logger(InMemoryDBStore.class);
 
@@ -83,6 +85,7 @@ public class InMemoryDBStore implements BackendStore {
         this.tables.put(type, table);
     }
 
+    @Override
     protected final InMemoryDBTable table(HugeType type) {
         assert type != null;
         InMemoryDBTable table = this.tables.get(type);
@@ -90,6 +93,16 @@ public class InMemoryDBStore implements BackendStore {
             throw new BackendException("Unsupported table type: %s", type);
         }
         return table;
+    }
+
+    @Override
+    protected BackendSession session(HugeType type) {
+        throw new UnsupportedOperationException("InMemoryDBStore.session()");
+    }
+
+    @Override
+    protected MetaDispatcher metaDispatcher() {
+        throw new UnsupportedOperationException("InMemoryDBStore.metaDispatcher()");
     }
 
     @Override

hugegraph-mysql/src/main/java/com/baidu/hugegraph/backend/store/mysql/MysqlStore.java
@@ -29,18 +29,20 @@ import org.slf4j.Logger;
 import com.baidu.hugegraph.backend.BackendException;
 import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.backend.query.Query;
+import com.baidu.hugegraph.backend.store.AbstractBackendStore;
 import com.baidu.hugegraph.backend.store.BackendAction;
 import com.baidu.hugegraph.backend.store.BackendEntry;
 import com.baidu.hugegraph.backend.store.BackendFeatures;
 import com.baidu.hugegraph.backend.store.BackendMutation;
-import com.baidu.hugegraph.backend.store.BackendStore;
+import com.baidu.hugegraph.backend.store.BackendSession;
 import com.baidu.hugegraph.backend.store.BackendStoreProvider;
+import com.baidu.hugegraph.backend.store.MetaDispatcher;
 import com.baidu.hugegraph.config.HugeConfig;
 import com.baidu.hugegraph.type.HugeType;
 import com.baidu.hugegraph.util.E;
 import com.baidu.hugegraph.util.Log;
 
-public abstract class MysqlStore implements BackendStore {
+public abstract class MysqlStore extends AbstractBackendStore {
 
     private static final Logger LOG = Log.logger(MysqlStore.class);
 
@@ -255,6 +257,11 @@ public abstract class MysqlStore implements BackendStore {
     }
 
     @Override
+    protected MetaDispatcher metaDispatcher() {
+        throw new UnsupportedOperationException("MysqlStore.metaDispatcher()");
+    }
+
+    @Override
     public BackendFeatures features() {
         return FEATURES;
     }
@@ -278,6 +285,7 @@ public abstract class MysqlStore implements BackendStore {
         }
     }
 
+    @Override
     protected final MysqlTable table(HugeType type) {
         assert type != null;
         MysqlTable table = this.tables.get(type);
@@ -285,6 +293,11 @@ public abstract class MysqlStore implements BackendStore {
             throw new BackendException("Unsupported table type: %s", type);
         }
         return table;
+    }
+
+    @Override
+    protected BackendSession session(HugeType type) {
+        return this.session(type);
     }
 
     protected final void checkClusterConnected() {

apache/incubator-hugegraph

hugegraph-core/src/main/java/com/baidu/hugegraph/backend/store/memory/InMemoryDBStore.java
@@ -30,13 +30,16 @@ import com.baidu.hugegraph.backend.LocalCounter;
 import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.backend.query.Query;
 import com.baidu.hugegraph.backend.serializer.TextBackendEntry;
+import com.baidu.hugegraph.backend.store.AbstractBackendStore;
 import com.baidu.hugegraph.backend.store.BackendAction;
 import com.baidu.hugegraph.backend.store.BackendEntry;
 import com.baidu.hugegraph.backend.store.BackendFeatures;
 import com.baidu.hugegraph.backend.store.BackendMutation;
-import com.baidu.hugegraph.backend.store.BackendStore;
+import com.baidu.hugegraph.backend.store.BackendSession;
 import com.baidu.hugegraph.backend.store.BackendStoreProvider;
+import com.baidu.hugegraph.backend.store.MetaDispatcher;
 import com.baidu.hugegraph.config.HugeConfig;
+import com.baidu.hugegraph.exception.NotSupportException;
 import com.baidu.hugegraph.type.HugeType;
 import com.baidu.hugegraph.util.Log;
 
@@ -53,7 +56,7 @@ import com.baidu.hugegraph.util.Log;
  * 1.remove by id + condition
  * 2.append/subtract edge-property
  */
-public class InMemoryDBStore implements BackendStore {
+public class InMemoryDBStore extends AbstractBackendStore {
 
     private static final Logger LOG = Log.logger(InMemoryDBStore.class);
 
@@ -76,13 +79,14 @@ public class InMemoryDBStore implements BackendStore {
 
     @Override
     public <R> R metadata(HugeType type, String meta, Object[] args) {
-        throw new UnsupportedOperationException("InMemoryDBStore.metadata()");
+        throw new NotSupportException("InMemoryDBStore.metadata()");
     }
 
     protected void registerTableManager(HugeType type, InMemoryDBTable table) {
         this.tables.put(type, table);
     }
 
+    @Override
     protected final InMemoryDBTable table(HugeType type) {
         assert type != null;
         InMemoryDBTable table = this.tables.get(type);
@@ -90,6 +94,16 @@ public class InMemoryDBStore implements BackendStore {
             throw new BackendException("Unsupported table type: %s", type);
         }
         return table;
+    }
+
+    @Override
+    protected BackendSession session(HugeType type) {
+        throw new NotSupportException("InMemoryDBStore.session()");
+    }
+
+    @Override
+    protected MetaDispatcher metaDispatcher() {
+        throw new NotSupportException("InMemoryDBStore.metaDispatcher()");
     }
 
     @Override

hugegraph-mysql/src/main/java/com/baidu/hugegraph/backend/store/mysql/MysqlStore.java
@@ -29,18 +29,20 @@ import org.slf4j.Logger;
 import com.baidu.hugegraph.backend.BackendException;
 import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.backend.query.Query;
+import com.baidu.hugegraph.backend.store.AbstractBackendStore;
 import com.baidu.hugegraph.backend.store.BackendAction;
 import com.baidu.hugegraph.backend.store.BackendEntry;
 import com.baidu.hugegraph.backend.store.BackendFeatures;
 import com.baidu.hugegraph.backend.store.BackendMutation;
-import com.baidu.hugegraph.backend.store.BackendStore;
 import com.baidu.hugegraph.backend.store.BackendStoreProvider;
+import com.baidu.hugegraph.backend.store.MetaDispatcher;
 import com.baidu.hugegraph.config.HugeConfig;
+import com.baidu.hugegraph.exception.NotSupportException;
 import com.baidu.hugegraph.type.HugeType;
 import com.baidu.hugegraph.util.E;
 import com.baidu.hugegraph.util.Log;
 
-public abstract class MysqlStore implements BackendStore {
+public abstract class MysqlStore extends AbstractBackendStore {
 
     private static final Logger LOG = Log.logger(MysqlStore.class);
 
@@ -251,7 +253,12 @@ public abstract class MysqlStore implements BackendStore {
 
     @Override
     public <R> R metadata(HugeType type, String meta, Object[] args) {
-        throw new UnsupportedOperationException("MysqlStore.metadata()");
+        throw new NotSupportException("MysqlStore.metadata()");
+    }
+
+    @Override
+    protected MetaDispatcher metaDispatcher() {
+        throw new NotSupportException("MysqlStore.metaDispatcher()");
     }
 
     @Override
@@ -278,6 +285,7 @@ public abstract class MysqlStore implements BackendStore {
         }
     }
 
+    @Override
     protected final MysqlTable table(HugeType type) {
         assert type != null;
         MysqlTable table = this.tables.get(type);
@@ -285,6 +293,12 @@ public abstract class MysqlStore implements BackendStore {
             throw new BackendException("Unsupported table type: %s", type);
         }
         return table;
+    }
+
+    @Override
+    protected MysqlSessions.Session session(HugeType type) {
+        this.checkSessionConnected();
+        return this.sessions.session();
     }
 
     protected final void checkClusterConnected() {