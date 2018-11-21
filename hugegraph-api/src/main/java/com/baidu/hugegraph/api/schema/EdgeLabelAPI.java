Bolinov/hugegraph

hugegraph-1379 support restore graph in restoring mode and merging mode
implement: #182

Change-Id: I7c2c85435af01c5d27279076d0473685b8f23f34

hugegraph-api/src/main/java/com/baidu/hugegraph/api/schema/EdgeLabelAPI.java
@@ -43,6 +43,7 @@ import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.core.GraphManager;
 import com.baidu.hugegraph.schema.EdgeLabel;
 import com.baidu.hugegraph.type.define.Frequency;
+import com.baidu.hugegraph.type.define.GraphMode;
 import com.baidu.hugegraph.util.E;
 import com.baidu.hugegraph.util.Log;
 import com.codahale.metrics.annotation.Timed;
@@ -178,6 +179,16 @@ public class EdgeLabelAPI extends API {
 
         private EdgeLabel.Builder convert2Builder(HugeGraph g) {
             EdgeLabel.Builder builder = g.schema().edgeLabel(this.name);
+            if (this.id != 0) {
+                E.checkArgument(this.id > 0,
+                                "Only positive number can be assign as " +
+                                "edge label id");
+                E.checkState(g.mode() == GraphMode.RESTORING,
+                             "Only accept edge label id when graph in " +
+                             "RESTORING mode, but '%s' is in mode '%s'",
+                             g, g.mode());
+                builder.id(this.id);
+            }
             if (this.sourceLabel != null) {
                 builder.sourceLabel(this.sourceLabel);
             }

hugegraph-core/src/main/java/com/baidu/hugegraph/backend/tx/SchemaTransaction.java
@@ -24,15 +24,19 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.tinkerpop.gremlin.structure.Graph;
+
 import com.baidu.hugegraph.HugeException;
 import com.baidu.hugegraph.HugeGraph;
 import com.baidu.hugegraph.backend.BackendException;
 import com.baidu.hugegraph.backend.id.Id;
+import com.baidu.hugegraph.backend.id.IdGenerator;
 import com.baidu.hugegraph.backend.query.ConditionQuery;
 import com.baidu.hugegraph.backend.query.Query;
 import com.baidu.hugegraph.backend.store.BackendEntry;
 import com.baidu.hugegraph.backend.store.BackendStore;
 import com.baidu.hugegraph.config.CoreOptions;
+import com.baidu.hugegraph.exception.ExistedException;
 import com.baidu.hugegraph.exception.NotAllowException;
 import com.baidu.hugegraph.job.JobBuilder;
 import com.baidu.hugegraph.job.schema.EdgeLabelRemoveCallable;
@@ -49,6 +53,7 @@ import com.baidu.hugegraph.schema.SchemaLabel;
 import com.baidu.hugegraph.schema.VertexLabel;
 import com.baidu.hugegraph.task.HugeTask;
 import com.baidu.hugegraph.type.HugeType;
+import com.baidu.hugegraph.type.define.GraphMode;
 import com.baidu.hugegraph.type.define.HugeKeys;
 import com.baidu.hugegraph.type.define.SchemaStatus;
 import com.baidu.hugegraph.util.E;
@@ -324,6 +329,17 @@ public class SchemaTransaction extends IndexableTransaction {
         return this.store().nextId(type);
     }
 
+    public Id getNextSystemId() {
+        LOG.debug("SchemaTransaction get next system id");
+        Id id = this.store().nextId(HugeType.SYS_SCHEMA);
+        return IdGenerator.of(-id.asLong());
+    }
+
+    public void setNextId(HugeType type, long lowest) {
+        LOG.debug("SchemaTransaction set next id to {} for {}", lowest, type);
+        this.store().setCounter(type, lowest);
+    }
+
     private BackendEntry serialize(SchemaElement schema) {
         switch (schema.type()) {
             case PROPERTY_KEY:
@@ -355,6 +371,57 @@ public class SchemaTransaction extends IndexableTransaction {
                 throw new AssertionError(String.format(
                           "Unknown schema type '%s'", type));
         }
+    }
+
+    public Id generateId(HugeType type, Id id, String name) {
+        if (id != null) {
+            id = validId(type, id, name);
+        } else {
+            if (Graph.Hidden.isHidden(name)) {
+                id = this.getNextSystemId();
+            } else {
+                id = this.getNextId(type);
+            }
+        }
+        return id;
+    }
+
+    public Id validId(HugeType type, Id id, String name) {
+        if (Graph.Hidden.isHidden(name)) {
+            if (id.number() && id.asLong() < 0) {
+                return id;
+            }
+            throw new IllegalStateException(String.format(
+                      "Invalid system id '%s'", id));
+        }
+        HugeGraph graph = this.graph();
+        E.checkState(id.number() && id.asLong() > 0L,
+                     "Schema id must be number and >0, but got '%s'", id);
+        E.checkState(graph.mode() == GraphMode.RESTORING,
+                     "Can't build schema with provided id '%s' " +
+                     "when graph '%s' in mode '%s'",
+                     id, graph, graph.mode());
+        this.setNextId(type, id.asLong());
+        return id;
+    }
+
+    public SchemaElement checkExist(HugeType type, String name,
+                                    Id id, boolean checkExist) {
+        SchemaElement element = this.getSchema(type, name);
+        if (element != null) {
+            if (checkExist) {
+                throw new ExistedException(type.toString(), name);
+            }
+            return element;
+        }
+        if (this.graph().mode() == GraphMode.RESTORING) {
+            E.checkNotNull(id, "id");
+            element = this.getSchema(type, id);
+            if (element != null) {
+                throw new ExistedException(type.toString() + " id", id);
+            }
+        }
+        return null;
     }
 
     private static Id asyncRun(HugeGraph graph, HugeType schemaType,

hugegraph-rocksdb/src/main/java/com/baidu/hugegraph/backend/store/rocksdb/RocksDBSessions.java
@@ -59,6 +59,7 @@ public abstract class RocksDBSessions extends BackendSessionPool {
 
         public abstract void put(String table, byte[] key, byte[] value);
         public abstract void merge(String table, byte[] key, byte[] value);
+        public abstract void mergeNow(String table, byte[] key, byte[] value);
 
         public abstract void remove(String table, byte[] key);
         public abstract void delete(String table, byte[] keyFrom, byte[] keyTo);

hugegraph-rocksdb/src/main/java/com/baidu/hugegraph/backend/store/rocksdbsst/RocksDBSstSessions.java
@@ -257,6 +257,14 @@ public class RocksDBSstSessions extends RocksDBSessions {
         }
 
         /**
+         * Merge a record to an existing key to a table and commit immediately
+         */
+        @Override
+        public void mergeNow(String table, byte[] key, byte[] value) {
+            throw new NotSupportException("RocksDBSstStore mergeNow()");
+        }
+
+        /**
          * Delete a record by key from a table
          */
         @Override

apache/incubator-hugegraph

hugegraph-api/src/main/java/com/baidu/hugegraph/api/schema/EdgeLabelAPI.java
@@ -43,6 +43,7 @@ import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.core.GraphManager;
 import com.baidu.hugegraph.schema.EdgeLabel;
 import com.baidu.hugegraph.type.define.Frequency;
+import com.baidu.hugegraph.type.define.GraphMode;
 import com.baidu.hugegraph.util.E;
 import com.baidu.hugegraph.util.Log;
 import com.codahale.metrics.annotation.Timed;
@@ -178,6 +179,16 @@ public class EdgeLabelAPI extends API {
 
         private EdgeLabel.Builder convert2Builder(HugeGraph g) {
             EdgeLabel.Builder builder = g.schema().edgeLabel(this.name);
+            if (this.id != 0) {
+                E.checkArgument(this.id > 0,
+                                "Only positive number can be assign as " +
+                                "edge label id");
+                E.checkArgument(g.mode() == GraphMode.RESTORING,
+                                "Only accept edge label id when graph in " +
+                                "RESTORING mode, but '%s' is in mode '%s'",
+                                g, g.mode());
+                builder.id(this.id);
+            }
             if (this.sourceLabel != null) {
                 builder.sourceLabel(this.sourceLabel);
             }

hugegraph-core/src/main/java/com/baidu/hugegraph/backend/tx/SchemaTransaction.java
@@ -24,15 +24,19 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.tinkerpop.gremlin.structure.Graph;
+
 import com.baidu.hugegraph.HugeException;
 import com.baidu.hugegraph.HugeGraph;
 import com.baidu.hugegraph.backend.BackendException;
 import com.baidu.hugegraph.backend.id.Id;
+import com.baidu.hugegraph.backend.id.IdGenerator;
 import com.baidu.hugegraph.backend.query.ConditionQuery;
 import com.baidu.hugegraph.backend.query.Query;
 import com.baidu.hugegraph.backend.store.BackendEntry;
 import com.baidu.hugegraph.backend.store.BackendStore;
 import com.baidu.hugegraph.config.CoreOptions;
+import com.baidu.hugegraph.exception.ExistedException;
 import com.baidu.hugegraph.exception.NotAllowException;
 import com.baidu.hugegraph.job.JobBuilder;
 import com.baidu.hugegraph.job.schema.EdgeLabelRemoveCallable;
@@ -49,6 +53,7 @@ import com.baidu.hugegraph.schema.SchemaLabel;
 import com.baidu.hugegraph.schema.VertexLabel;
 import com.baidu.hugegraph.task.HugeTask;
 import com.baidu.hugegraph.type.HugeType;
+import com.baidu.hugegraph.type.define.GraphMode;
 import com.baidu.hugegraph.type.define.HugeKeys;
 import com.baidu.hugegraph.type.define.SchemaStatus;
 import com.baidu.hugegraph.util.E;
@@ -328,6 +333,17 @@ public class SchemaTransaction extends IndexableTransaction {
         return this.store().nextId(type);
     }
 
+    public void setNextIdLowest(HugeType type, long lowest) {
+        LOG.debug("SchemaTransaction set next id to {} for {}", lowest, type);
+        this.store().setCounterLowest(type, lowest);
+    }
+
+    public Id getNextSystemId() {
+        LOG.debug("SchemaTransaction get next system id");
+        Id id = this.store().nextId(HugeType.SYS_SCHEMA);
+        return IdGenerator.of(-id.asLong());
+    }
+
     private BackendEntry serialize(SchemaElement schema) {
         switch (schema.type()) {
             case PROPERTY_KEY:
@@ -358,6 +374,50 @@ public class SchemaTransaction extends IndexableTransaction {
             default:
                 throw new AssertionError(String.format(
                           "Unknown schema type '%s'", type));
+        }
+    }
+
+    public Id validOrGenerateId(HugeType type, Id id, String name) {
+        boolean forSystem = Graph.Hidden.isHidden(name);
+        if (id != null) {
+            this.checkIdAndUpdateNextId(type, id, name, forSystem);
+        } else {
+            if (forSystem) {
+                id = this.getNextSystemId();
+            } else {
+                id = this.getNextId(type);
+            }
+        }
+        return id;
+    }
+
+    private void checkIdAndUpdateNextId(HugeType type, Id id,
+                                        String name, boolean forSystem) {
+        if (forSystem) {
+            if (id.number() && id.asLong() < 0) {
+                return;
+            }
+            throw new IllegalStateException(String.format(
+                      "Invalid system id '%s'", id));
+        }
+        HugeGraph graph = this.graph();
+        E.checkState(id.number() && id.asLong() > 0L,
+                     "Schema id must be number and >0, but got '%s'", id);
+        E.checkState(graph.mode() == GraphMode.RESTORING,
+                     "Can't build schema with provided id '%s' " +
+                     "when graph '%s' in mode '%s'",
+                     id, graph, graph.mode());
+        this.setNextIdLowest(type, id.asLong());
+    }
+
+    public void checkIdIfRestoringMode(HugeType type, Id id) {
+        if (this.graph().mode() == GraphMode.RESTORING) {
+            E.checkArgument(id != null,
+                            "Must provide schema id if in RESTORING mode");
+            SchemaElement element = this.getSchema(type, id);
+            if (element != null) {
+                throw new ExistedException(type.toString() + " id", id);
+            }
         }
     }
 

hugegraph-rocksdb/src/main/java/com/baidu/hugegraph/backend/store/rocksdb/RocksDBSessions.java
@@ -59,6 +59,7 @@ public abstract class RocksDBSessions extends BackendSessionPool {
 
         public abstract void put(String table, byte[] key, byte[] value);
         public abstract void merge(String table, byte[] key, byte[] value);
+        public abstract void increase(String table, byte[] key, byte[] value);
 
         public abstract void remove(String table, byte[] key);
         public abstract void delete(String table, byte[] keyFrom, byte[] keyTo);

hugegraph-rocksdb/src/main/java/com/baidu/hugegraph/backend/store/rocksdbsst/RocksDBSstSessions.java
@@ -257,6 +257,14 @@ public class RocksDBSstSessions extends RocksDBSessions {
         }
 
         /**
+         * Merge a record to an existing key to a table and commit immediately
+         */
+        @Override
+        public void increase(String table, byte[] key, byte[] value) {
+            throw new NotSupportException("RocksDBSstStore increase()");
+        }
+
+        /**
          * Delete a record by key from a table
          */
         @Override