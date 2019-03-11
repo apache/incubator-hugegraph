songhtdo/hugegraph

implement customized path and crosspoints API
implemented: #305

Change-Id: I31edf93fafc91854ffe7b9ec36c97aeda1b18088

hugegraph-core/src/main/java/com/baidu/hugegraph/traversal/algorithm/HugeTraverser.java
@@ -1,0 +1,414 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.baidu.hugegraph.traversal.algorithm;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.tinkerpop.gremlin.process.traversal.P;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+
+import com.baidu.hugegraph.HugeGraph;
+import com.baidu.hugegraph.backend.id.Id;
+import com.baidu.hugegraph.backend.query.Query;
+import com.baidu.hugegraph.backend.tx.GraphTransaction;
+import com.baidu.hugegraph.iterator.ExtendableIterator;
+import com.baidu.hugegraph.rest.ClientException;
+import com.baidu.hugegraph.schema.SchemaLabel;
+import com.baidu.hugegraph.structure.HugeEdge;
+import com.baidu.hugegraph.type.HugeType;
+import com.baidu.hugegraph.type.define.Directions;
+import com.baidu.hugegraph.util.E;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+public class HugeTraverser {
+
+    private HugeGraph graph;
+
+    public static final List<Id> PATH_NONE = ImmutableList.of();
+
+    public static final String DEFAULT_CAPACITY = "10000000";
+    public static final String DEFAULT_ELEMENTS_LIMIT = "10000000";
+    public static final String DEFAULT_PATHS_LIMIT = "10";
+    public static final String DEFAULT_DEGREE = "10000";
+    public static final String DEFAULT_SAMPLE = "100";
+    public static final String DEFAULT_WEIGHT = "0";
+
+    public static final long NO_LIMIT = -1L;
+
+    public HugeTraverser(HugeGraph graph) {
+        this.graph = graph;
+    }
+
+    public HugeGraph graph() {
+        return this.graph;
+    }
+
+    public Set<Id> kout(Id sourceV, Directions dir, String label,
+                        int depth, boolean nearest,
+                        long degree, long capacity, long limit) {
+        E.checkNotNull(sourceV, "source vertex id");
+        E.checkNotNull(dir, "direction");
+        checkPositive(depth, "k-out depth");
+        checkDegree(degree);
+        checkCapacity(capacity);
+        checkLimit(limit);
+        if (capacity != NO_LIMIT) {
+            // Capacity must > limit because sourceV is counted in capacity
+            E.checkArgument(capacity > limit && limit != NO_LIMIT,
+                            "Capacity must be greater than limit, " +
+                            "but got capacity '%s' and limit '%s'",
+                            capacity, limit);
+        }
+
+        Id labelId = this.getEdgeLabelId(label);
+
+        Set<Id> latest = newSet();
+        latest.add(sourceV);
+
+        Set<Id> all = newSet();
+        all.add(sourceV);
+
+        long remaining = capacity == NO_LIMIT ?
+                         NO_LIMIT : capacity - latest.size();
+        while (depth-- > 0) {
+            // Just get limit nodes in last layer if limit < remaining capacity
+            if (depth == 0 && limit != NO_LIMIT &&
+                (limit < remaining || remaining == NO_LIMIT)) {
+                remaining = limit;
+            }
+            if (nearest) {
+                latest = this.adjacentVertices(latest, dir, labelId, all,
+                                               degree, remaining);
+                all.addAll(latest);
+            } else {
+                latest = this.adjacentVertices(latest, dir, labelId, null,
+                                               degree, remaining);
+            }
+            if (capacity != NO_LIMIT) {
+                // Update 'remaining' value to record remaining capacity
+                remaining -= latest.size();
+
+                if (remaining <= 0 && depth > 0) {
+                    throw new ClientException(
+                              "Reach limit '%s' while remaining depth '%s'",
+                              limit, depth);
+                }
+            }
+        }
+
+        return latest;
+    }
+
+    public Set<Id> kneighbor(Id sourceV, Directions dir,
+                             String label, int depth,
+                             long degree, long limit) {
+        E.checkNotNull(sourceV, "source vertex id");
+        E.checkNotNull(dir, "direction");
+        checkPositive(depth, "k-neighbor depth");
+        checkDegree(degree);
+        checkLimit(limit);
+
+        Id labelId = this.getEdgeLabelId(label);
+
+        Set<Id> latest = newSet();
+        latest.add(sourceV);
+
+        Set<Id> all = newSet();
+        all.add(sourceV);
+
+        while (depth-- > 0) {
+            long remaining = limit == NO_LIMIT ? NO_LIMIT : limit - all.size();
+            latest = this.adjacentVertices(latest, dir, labelId, all,
+                                           degree, remaining);
+            all.addAll(latest);
+            if (limit != NO_LIMIT && all.size() >= limit) {
+                break;
+            }
+        }
+
+        return all;
+    }
+
+    private Set<Id> adjacentVertices(Set<Id> vertices, Directions dir,
+                                     Id label, Set<Id> excluded,
+                                     long degree, long limit) {
+        if (limit == 0) {
+            return ImmutableSet.of();
+        }
+
+        Set<Id> neighbors = newSet();
+        for (Id source : vertices) {
+            Iterator<Edge> edges = this.edgesOfVertex(source, dir,
+                                                      label, degree);
+            while (edges.hasNext()) {
+                HugeEdge e = (HugeEdge) edges.next();
+                Id target = e.id().otherVertexId();
+                if (excluded != null && excluded.contains(target)) {
+                    continue;
+                }
+                neighbors.add(target);
+                if (limit != NO_LIMIT && neighbors.size() >= limit) {
+                    return neighbors;
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    protected Iterator<Edge> edgesOfVertex(Id source, Directions dir,
+                                           Id label, long limit) {
+        Id[] labels = {};
+        if (label != null) {
+            labels = new Id[]{label};
+        }
+
+        Query query = GraphTransaction.constructEdgesQuery(source, dir, labels);
+        if (limit != NO_LIMIT) {
+            query.limit(limit);
+        }
+        return this.graph.edges(query);
+    }
+
+    protected Iterator<Edge> edgesOfVertex(Id source, Directions dir,
+                                           Set<Id> labels, long limit) {
+        if (labels == null || labels.isEmpty()) {
+            return this.edgesOfVertex(source, dir, (Id) null, limit);
+        }
+        ExtendableIterator<Edge> results = new ExtendableIterator<>();
+        for (Id label : labels) {
+            E.checkNotNull(label, "edge label");
+            results.extend(this.edgesOfVertex(source, dir, label, limit));
+        }
+        return results;
+    }
+
+    protected Iterator<Edge> edgesOfVertex(Id source, Directions dir,
+                                           Map<Id, String> labels,
+                                           Map<String, Object> properties,
+                                           long limit) {
+        if (properties == null || properties.isEmpty()) {
+            return edgesOfVertex(source, dir, labels.keySet(), limit);
+        }
+        // Use traversal format if has properties filter
+        String[] els = labels.values().toArray(new String[labels.size()]);
+        GraphTraversal<Vertex, Edge> g;
+        g = this.graph().traversal().V(source).toE(dir.direction(), els);
+        for (Map.Entry<String, Object> entry : properties.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (value instanceof List) {
+                g = g.has(key, P.within((Collection<?>) value));
+            } else {
+                g = g.has(key, value);
+            }
+        }
+        return g.limit(limit);
+    }
+
+    protected Id getEdgeLabelId(Object label) {
+        if (label == null) {
+            return null;
+        }
+        return SchemaLabel.getLabelId(this.graph, HugeType.EDGE, label);
+    }
+
+    protected static void checkPositive(int value, String name) {
+        E.checkArgument(value > 0,
+                        "The %s parameter must be > 0, but got '%s'",
+                        name, value);
+    }
+
+    protected static void checkDegree(long degree) {
+        checkPositiveOrNoLimit(degree, "max degree");
+    }
+
+    protected static void checkCapacity(long capacity) {
+        checkPositiveOrNoLimit(capacity, "capacity");
+    }
+
+    protected static void checkLimit(long limit) {
+        checkPositiveOrNoLimit(limit, "limit");
+    }
+
+    protected static void checkPositiveOrNoLimit(long value, String name) {
+        E.checkArgument(value > 0 || value == NO_LIMIT,
+                        "The %s parameter must be > 0 or == %s, but got: %s",
+                        name, NO_LIMIT, value);
+    }
+
+    protected static <V> Set<V> newSet() {
+        return new HashSet<>();
+    }
+
+    protected static <K, V> Map<K, V> newMap() {
+        return new HashMap<>();
+    }
+
+    protected static <K, V> MultivaluedMap<K, V> newMultivalueMap() {
+        return new MultivaluedHashMap<>();
+    }
+
+    public static class Node {
+
+        private Id id;
+        private Node parent;
+
+        public Node(Id id) {
+            this(id, null);
+        }
+
+        public Node(Id id, Node parent) {
+            E.checkArgumentNotNull(id, "Id of Node can't be null");
+            this.id = id;
+            this.parent = parent;
+        }
+
+        public Id id() {
+            return this.id;
+        }
+
+        @SuppressWarnings("unused")
+        public Node parent() {
+            return this.parent;
+        }
+
+        public List<Id> path() {
+            List<Id> ids = new ArrayList<>();
+            Node current = this;
+            do {
+                ids.add(current.id);
+                current = current.parent;
+            } while (current != null);
+            Collections.reverse(ids);
+            return ids;
+        }
+
+        public List<Id> joinPath(Node back) {
+            // Get self path
+            List<Id> path = this.path();
+
+            // Get reversed other path
+            List<Id> backPath = back.path();
+            Collections.reverse(backPath);
+
+            // Avoid loop in path
+            if (CollectionUtils.containsAny(path, backPath)) {
+                return ImmutableList.of();
+            }
+
+            // Append other path behind self path
+            path.addAll(backPath);
+            return path;
+        }
+
+        public boolean contains(Id id) {
+            Node node = this;
+            do {
+                if (node.id.equals(id)) {
+                    return true;
+                }
+                node = node.parent;
+            } while (node != null);
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.id.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (!(object instanceof Node)) {
+                return false;
+            }
+            Node other = (Node) object;
+            return this.id.equals(other.id);
+        }
+    }
+
+    public static class Path {
+
+        private Id crosspoint;
+        private List<Id> vertices;
+
+        public Path(Id crosspoint, List<Id> vertices) {
+            this.crosspoint = crosspoint;
+            this.vertices = vertices;
+        }
+
+        public Id crosspoint() {
+            return this.crosspoint;
+        }
+
+        public List<Id> vertices() {
+            return this.vertices;
+        }
+
+        public void reverse() {
+            Collections.reverse(this.vertices);
+        }
+
+        public Map<String, Object> toMap(boolean withCrossPoint) {
+            if (withCrossPoint) {
+                return ImmutableMap.of("crosspoint", this.crosspoint,
+                                       "objects", this.vertices);
+            } else {
+                return ImmutableMap.of("objects", this.vertices);
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return this.vertices.hashCode();
+        }
+
+        /**
+         * Compares the specified object with this path for equality.
+         * Returns <tt>true</tt> if and only if both have same vertices list
+         * without regard of crosspoint.
+         * @param other the object to be compared for equality with this path
+         * @return <tt>true</tt> if the specified object is equal to this path
+         */
+        @Override
+        public boolean equals(Object other) {
+            if (other == null || !(other instanceof Path)) {
+                return false;
+            }
+            return this.vertices.equals(((Path) other).vertices);
+        }
+    }
+}

apache/incubator-hugegraph

hugegraph-core/src/main/java/com/baidu/hugegraph/traversal/algorithm/HugeTraverser.java
@@ -1,0 +1,422 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.baidu.hugegraph.traversal.algorithm;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.tinkerpop.gremlin.process.traversal.P;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+
+import com.baidu.hugegraph.HugeException;
+import com.baidu.hugegraph.HugeGraph;
+import com.baidu.hugegraph.backend.id.Id;
+import com.baidu.hugegraph.backend.query.Query;
+import com.baidu.hugegraph.backend.tx.GraphTransaction;
+import com.baidu.hugegraph.iterator.ExtendableIterator;
+import com.baidu.hugegraph.schema.SchemaLabel;
+import com.baidu.hugegraph.structure.HugeEdge;
+import com.baidu.hugegraph.type.HugeType;
+import com.baidu.hugegraph.type.define.Directions;
+import com.baidu.hugegraph.util.E;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+public class HugeTraverser {
+
+    private HugeGraph graph;
+
+    public static final List<Id> PATH_NONE = ImmutableList.of();
+
+    public static final String DEFAULT_CAPACITY = "10000000";
+    public static final String DEFAULT_ELEMENTS_LIMIT = "10000000";
+    public static final String DEFAULT_PATHS_LIMIT = "10";
+    public static final String DEFAULT_DEGREE = "10000";
+    public static final String DEFAULT_SAMPLE = "100";
+    public static final String DEFAULT_WEIGHT = "0";
+
+    public static final long NO_LIMIT = -1L;
+
+    public HugeTraverser(HugeGraph graph) {
+        this.graph = graph;
+    }
+
+    public HugeGraph graph() {
+        return this.graph;
+    }
+
+    public Set<Id> kout(Id sourceV, Directions dir, String label,
+                        int depth, boolean nearest,
+                        long degree, long capacity, long limit) {
+        E.checkNotNull(sourceV, "source vertex id");
+        E.checkNotNull(dir, "direction");
+        checkPositive(depth, "k-out depth");
+        checkDegree(degree);
+        checkCapacity(capacity);
+        checkLimit(limit);
+        if (capacity != NO_LIMIT) {
+            // Capacity must > limit because sourceV is counted in capacity
+            E.checkArgument(capacity >= limit && limit != NO_LIMIT,
+                            "Capacity can't be less than limit, " +
+                            "but got capacity '%s' and limit '%s'",
+                            capacity, limit);
+        }
+
+        Id labelId = this.getEdgeLabelId(label);
+
+        Set<Id> latest = newSet();
+        latest.add(sourceV);
+
+        Set<Id> all = newSet();
+        all.add(sourceV);
+
+        long remaining = capacity == NO_LIMIT ?
+                         NO_LIMIT : capacity - latest.size();
+        while (depth-- > 0) {
+            // Just get limit nodes in last layer if limit < remaining capacity
+            if (depth == 0 && limit != NO_LIMIT &&
+                (limit < remaining || remaining == NO_LIMIT)) {
+                remaining = limit;
+            }
+            if (nearest) {
+                latest = this.adjacentVertices(latest, dir, labelId, all,
+                                               degree, remaining);
+                all.addAll(latest);
+            } else {
+                latest = this.adjacentVertices(latest, dir, labelId, null,
+                                               degree, remaining);
+            }
+            if (capacity != NO_LIMIT) {
+                // Update 'remaining' value to record remaining capacity
+                remaining -= latest.size();
+
+                if (remaining <= 0 && depth > 0) {
+                    throw new HugeException(
+                              "Reach capacity '%s' while remaining depth '%s'",
+                              capacity, depth);
+                }
+            }
+        }
+
+        return latest;
+    }
+
+    public Set<Id> kneighbor(Id sourceV, Directions dir,
+                             String label, int depth,
+                             long degree, long limit) {
+        E.checkNotNull(sourceV, "source vertex id");
+        E.checkNotNull(dir, "direction");
+        checkPositive(depth, "k-neighbor depth");
+        checkDegree(degree);
+        checkLimit(limit);
+
+        Id labelId = this.getEdgeLabelId(label);
+
+        Set<Id> latest = newSet();
+        latest.add(sourceV);
+
+        Set<Id> all = newSet();
+        all.add(sourceV);
+
+        while (depth-- > 0) {
+            long remaining = limit == NO_LIMIT ? NO_LIMIT : limit - all.size();
+            latest = this.adjacentVertices(latest, dir, labelId, all,
+                                           degree, remaining);
+            all.addAll(latest);
+            if (limit != NO_LIMIT && all.size() >= limit) {
+                break;
+            }
+        }
+
+        return all;
+    }
+
+    private Set<Id> adjacentVertices(Set<Id> vertices, Directions dir,
+                                     Id label, Set<Id> excluded,
+                                     long degree, long limit) {
+        if (limit == 0) {
+            return ImmutableSet.of();
+        }
+
+        Set<Id> neighbors = newSet();
+        for (Id source : vertices) {
+            Iterator<Edge> edges = this.edgesOfVertex(source, dir,
+                                                      label, degree);
+            while (edges.hasNext()) {
+                HugeEdge e = (HugeEdge) edges.next();
+                Id target = e.id().otherVertexId();
+                if (excluded != null && excluded.contains(target)) {
+                    continue;
+                }
+                neighbors.add(target);
+                if (limit != NO_LIMIT && neighbors.size() >= limit) {
+                    return neighbors;
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    protected Iterator<Edge> edgesOfVertex(Id source, Directions dir,
+                                           Id label, long limit) {
+        Id[] labels = {};
+        if (label != null) {
+            labels = new Id[]{label};
+        }
+
+        Query query = GraphTransaction.constructEdgesQuery(source, dir, labels);
+        if (limit != NO_LIMIT) {
+            query.limit(limit);
+        }
+        return this.graph.edges(query);
+    }
+
+    protected Iterator<Edge> edgesOfVertex(Id source, Directions dir,
+                                           Set<Id> labels, long limit) {
+        if (labels == null || labels.isEmpty()) {
+            return this.edgesOfVertex(source, dir, (Id) null, limit);
+        }
+        ExtendableIterator<Edge> results = new ExtendableIterator<>();
+        for (Id label : labels) {
+            E.checkNotNull(label, "edge label");
+            results.extend(this.edgesOfVertex(source, dir, label, limit));
+        }
+        return results;
+    }
+
+    protected Iterator<Edge> edgesOfVertex(Id source, Directions dir,
+                                           Map<Id, String> labels,
+                                           Map<String, Object> properties,
+                                           long limit) {
+        if (properties == null || properties.isEmpty()) {
+            return edgesOfVertex(source, dir, labels.keySet(), limit);
+        }
+        // Use traversal format if has properties filter
+        String[] els = labels.values().toArray(new String[labels.size()]);
+        GraphTraversal<Vertex, Edge> g;
+        g = this.graph().traversal().V(source).toE(dir.direction(), els);
+        for (Map.Entry<String, Object> entry : properties.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (value instanceof List) {
+                g = g.has(key, P.within((Collection<?>) value));
+            } else {
+                g = g.has(key, value);
+            }
+        }
+        return g.limit(limit);
+    }
+
+    protected Id getEdgeLabelId(Object label) {
+        if (label == null) {
+            return null;
+        }
+        return SchemaLabel.getLabelId(this.graph, HugeType.EDGE, label);
+    }
+
+    protected static void checkPositive(int value, String name) {
+        E.checkArgument(value > 0,
+                        "The %s parameter must be > 0, but got '%s'",
+                        name, value);
+    }
+
+    protected static void checkDegree(long degree) {
+        checkPositiveOrNoLimit(degree, "max degree");
+    }
+
+    protected static void checkCapacity(long capacity) {
+        checkPositiveOrNoLimit(capacity, "capacity");
+    }
+
+    protected static void checkLimit(long limit) {
+        checkPositiveOrNoLimit(limit, "limit");
+    }
+
+    protected static void checkPositiveOrNoLimit(long value, String name) {
+        E.checkArgument(value > 0 || value == NO_LIMIT,
+                        "The %s parameter must be > 0 or == %s, but got: %s",
+                        name, NO_LIMIT, value);
+    }
+
+    protected static void checkCapacity(long capacity, long access,
+                                        String traverse) {
+        if (capacity != NO_LIMIT && access > capacity) {
+            throw new HugeException("Exceed capacity '%s' while finding %s",
+                                    capacity, traverse);
+        }
+    }
+
+    protected static <V> Set<V> newSet() {
+        return new HashSet<>();
+    }
+
+    protected static <K, V> Map<K, V> newMap() {
+        return new HashMap<>();
+    }
+
+    protected static <K, V> MultivaluedMap<K, V> newMultivalueMap() {
+        return new MultivaluedHashMap<>();
+    }
+
+    public static class Node {
+
+        private Id id;
+        private Node parent;
+
+        public Node(Id id) {
+            this(id, null);
+        }
+
+        public Node(Id id, Node parent) {
+            E.checkArgumentNotNull(id, "Id of Node can't be null");
+            this.id = id;
+            this.parent = parent;
+        }
+
+        public Id id() {
+            return this.id;
+        }
+
+        @SuppressWarnings("unused")
+        public Node parent() {
+            return this.parent;
+        }
+
+        public List<Id> path() {
+            List<Id> ids = new ArrayList<>();
+            Node current = this;
+            do {
+                ids.add(current.id);
+                current = current.parent;
+            } while (current != null);
+            Collections.reverse(ids);
+            return ids;
+        }
+
+        public List<Id> joinPath(Node back) {
+            // Get self path
+            List<Id> path = this.path();
+
+            // Get reversed other path
+            List<Id> backPath = back.path();
+            Collections.reverse(backPath);
+
+            // Avoid loop in path
+            if (CollectionUtils.containsAny(path, backPath)) {
+                return ImmutableList.of();
+            }
+
+            // Append other path behind self path
+            path.addAll(backPath);
+            return path;
+        }
+
+        public boolean contains(Id id) {
+            Node node = this;
+            do {
+                if (node.id.equals(id)) {
+                    return true;
+                }
+                node = node.parent;
+            } while (node != null);
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.id.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (!(object instanceof Node)) {
+                return false;
+            }
+            Node other = (Node) object;
+            return this.id.equals(other.id);
+        }
+    }
+
+    public static class Path {
+
+        private Id crosspoint;
+        private List<Id> vertices;
+
+        public Path(Id crosspoint, List<Id> vertices) {
+            this.crosspoint = crosspoint;
+            this.vertices = vertices;
+        }
+
+        public Id crosspoint() {
+            return this.crosspoint;
+        }
+
+        public List<Id> vertices() {
+            return this.vertices;
+        }
+
+        public void reverse() {
+            Collections.reverse(this.vertices);
+        }
+
+        public Map<String, Object> toMap(boolean withCrossPoint) {
+            if (withCrossPoint) {
+                return ImmutableMap.of("crosspoint", this.crosspoint,
+                                       "objects", this.vertices);
+            } else {
+                return ImmutableMap.of("objects", this.vertices);
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return this.vertices.hashCode();
+        }
+
+        /**
+         * Compares the specified object with this path for equality.
+         * Returns <tt>true</tt> if and only if both have same vertices list
+         * without regard of crosspoint.
+         * @param other the object to be compared for equality with this path
+         * @return <tt>true</tt> if the specified object is equal to this path
+         */
+        @Override
+        public boolean equals(Object other) {
+            if (other == null || !(other instanceof Path)) {
+                return false;
+            }
+            return this.vertices.equals(((Path) other).vertices);
+        }
+    }
+}