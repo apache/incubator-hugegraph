bed8a6ea03b7867dbacfd010d9688dbba5059c50
Linary <liningrui@vip.qq.com>
2019-05-07 11:43:01 +0800

    Bump up to version 0.10.0 (#471)
    Change-Id: I49314313781c73436e3370d2a6334adfbc142d96

--- hugegraph-api/src/main/java/com/baidu/hugegraph/version/ApiVersion.java
+++ hugegraph-api/src/main/java/com/baidu/hugegraph/version/ApiVersion.java
@@ -91,6 +91,6 @@ public final class ApiVersion {
 
     public static final void check() {
         // Check version of hugegraph-core. Firstly do check from version 0.3
-        VersionUtil.check(CoreVersion.VERSION, "0.9", "0.10", CoreVersion.NAME);
+        VersionUtil.check(CoreVersion.VERSION, "0.10", "0.11", CoreVersion.NAME);
     }
 }
