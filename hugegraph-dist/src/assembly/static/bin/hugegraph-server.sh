fan0916/hugegraph

Not allowed perform sensitive operations via gremlin
Implement #145

Change-Id: I9a590fe40d3b5a808b569ed0af8fd83214a2941a

hugegraph-dist/src/assembly/static/bin/hugegraph-server.sh
@@ -19,6 +19,7 @@ PLUGINS="$TOP/plugins"
 LOG="$TOP/logs"
 OUTPUT=${LOG}/hugegraph-server.log
 
+export HUGEGRAPH_HOME="$TOP"
 . ${BIN}/util.sh
 
 ensure_path_writable $LOG
@@ -79,6 +80,9 @@ fi
 # Execute the application and return its exit code
 ARGS="conf/gremlin-server.yaml conf/rest-server.properties"
 
-exec ${JAVA} -Dname="HugeGraphServer" -Dlog4j.configurationFile="${CONF}/log4j2.xml" \
+# Turn on security check
+exec ${JAVA} -Dname="HugeGraphServer" \
+-Djava.security.manager="com.baidu.hugegraph.dist.HugeSecurityManager" \
+-Dlog4j.configurationFile="${CONF}/log4j2.xml" \
 ${JAVA_OPTIONS} -cp ${CLASSPATH}: com.baidu.hugegraph.dist.HugeGraphServer ${ARGS} \
 >> ${OUTPUT} 2>&1

apache/incubator-hugegraph

hugegraph-dist/src/assembly/static/bin/hugegraph-server.sh
@@ -19,6 +19,7 @@ PLUGINS="$TOP/plugins"
 LOG="$TOP/logs"
 OUTPUT=${LOG}/hugegraph-server.log
 
+export HUGEGRAPH_HOME="$TOP"
 . ${BIN}/util.sh
 
 ensure_path_writable $LOG
@@ -79,6 +80,9 @@ fi
 # Execute the application and return its exit code
 ARGS="conf/gremlin-server.yaml conf/rest-server.properties"
 
-exec ${JAVA} -Dname="HugeGraphServer" -Dlog4j.configurationFile="${CONF}/log4j2.xml" \
+# Turn on security check
+exec ${JAVA} -Dname="HugeGraphServer" \
+-Djava.security.manager="com.baidu.hugegraph.security.HugeSecurityManager" \
+-Dlog4j.configurationFile="${CONF}/log4j2.xml" \
 ${JAVA_OPTIONS} -cp ${CLASSPATH}: com.baidu.hugegraph.dist.HugeGraphServer ${ARGS} \
 >> ${OUTPUT} 2>&1