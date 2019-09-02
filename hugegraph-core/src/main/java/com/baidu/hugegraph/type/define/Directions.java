1c99b1bbaa935e02d9a606288d02c57b8b0b6b99
liningrui <liningrui@baidu.com>
2019-09-02 10:39:04 +0800

    tiny improve
    Change-Id: I762be56a3dea63f0b26c654ed0da7f6da726b717

--- hugegraph-core/src/main/java/com/baidu/hugegraph/type/define/Directions.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/type/define/Directions.java
@@ -38,7 +38,7 @@ public enum Directions implements SerialEnum {
 
     public HugeType type() {
         switch (this) {
-            case BOTH:
+//            case BOTH:
             case OUT:
                 return HugeType.EDGE_OUT;
             case IN:
