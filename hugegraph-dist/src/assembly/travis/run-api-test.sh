89cf5ffb8801ea988ab76778059683529674df93
zhangyi51 <zhangyi51@baidu.com>
2019-12-05 22:41:47 +0800

    add cat log if api-test failed
    Change-Id: Iaa99ba2a6dfb9fccac7e4ee0f8fd07cdd05b4f99

--- hugegraph-dist/src/assembly/travis/run-api-test.sh
+++ hugegraph-dist/src/assembly/travis/run-api-test.sh
@@ -3,9 +3,11 @@
 set -ev
 
 TRAVIS_DIR=`dirname $0`
+VERSION=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
+SERVER_DIR=hugegraph-$VERSION
 
 mvn package -DskipTests
-$TRAVIS_DIR/start-server.sh
-mvn test -P api-test,$BACKEND || cat hugegraph-0.11.0/logs/hugegraph-server.log
+$TRAVIS_DIR/start-server.sh $VERSION
+mvn test -P api-test,$BACKEND || cat $SERVER_DIR/logs/hugegraph-server.log
 $TRAVIS_DIR/build-report.sh
 $TRAVIS_DIR/stop-server.sh
