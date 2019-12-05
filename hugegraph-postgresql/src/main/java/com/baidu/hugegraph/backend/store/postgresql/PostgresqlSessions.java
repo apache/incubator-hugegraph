c5432fdaa7753059ada56fadaffbd547db5f52e7
zhangyi51 <zhangyi51@baidu.com>
2019-12-05 11:27:19 +0800

    remove pg redundant methods of drop database
    Change-Id: I0816800290fc50cd566ff84452f9a40a70cec789

--- hugegraph-postgresql/src/main/java/com/baidu/hugegraph/backend/store/postgresql/PostgresqlSessions.java
+++ hugegraph-postgresql/src/main/java/com/baidu/hugegraph/backend/store/postgresql/PostgresqlSessions.java
@@ -89,17 +89,6 @@ public class PostgresqlSessions extends MysqlSessions {
     }
 
     @Override
-    public void dropDatabase() {
-        /*
-         * Issue #758.
-         * PostgreSQL drop database should terminate all other connections to
-         * the database, which will make other alive sessions terminated.
-         * TODO: PostgreSQL clear backend close all sessions
-         */
-        LOG.warn("Skip drop database: {}", this.database());
-    }
-
-    @Override
     protected String buildCreateDatabase(String database) {
         return String.format(POSTGRESQL_DB_CREATE, database);
     }
--- hugegraph-postgresql/src/main/java/com/baidu/hugegraph/backend/store/postgresql/PostgresqlStoreProvider.java
+++ hugegraph-postgresql/src/main/java/com/baidu/hugegraph/backend/store/postgresql/PostgresqlStoreProvider.java
@@ -21,10 +21,8 @@ package com.baidu.hugegraph.backend.store.postgresql;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
-import com.baidu.hugegraph.backend.BackendException;
 import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.backend.store.BackendStore;
 import com.baidu.hugegraph.backend.store.BackendStoreProvider;
@@ -33,7 +31,6 @@ import com.baidu.hugegraph.backend.store.mysql.MysqlStoreProvider;
 import com.baidu.hugegraph.backend.store.mysql.MysqlTable;
 import com.baidu.hugegraph.type.HugeType;
 import com.baidu.hugegraph.type.define.Directions;
-import com.baidu.hugegraph.util.Events;
 
 public class PostgresqlStoreProvider extends MysqlStoreProvider {
 
@@ -50,20 +47,6 @@ public class PostgresqlStoreProvider extends MysqlStoreProvider {
     @Override
     public String type() {
         return "postgresql";
-    }
-
-    @Override
-    public void clear() throws BackendException {
-        this.checkOpened();
-        /*
-         * Issue #758: We should clear graph/schema/system store one by one to
-         * drop all tables because database will not be dropped
-         */
-        Iterator<BackendStore> iter = this.stores.values().iterator();
-        while (iter.hasNext()) {
-            iter.next().clear();
-        }
-        this.notifyAndWaitEvent(Events.STORE_CLEAR);
     }
 
     @Override
