d1d0748a1db8fcad8b13148f9493b05d06a504d7
zhangyi51 <zhangyi51@baidu.com>
2019-12-05 21:57:14 +0800

    test hbase classloader
    Change-Id: Ice93e0103bc2bf486c91573972e50b282b8335f0

--- hugegraph-dist/src/assembly/travis/run-api-test.sh
+++ hugegraph-dist/src/assembly/travis/run-api-test.sh
@@ -6,7 +6,6 @@ TRAVIS_DIR=`dirname $0`
 
 mvn package -DskipTests
 $TRAVIS_DIR/start-server.sh
-mvn test -P api-test,$BACKEND
-cat hugegraph-0.11.0/logs/hugegraph-server.log
+mvn test -P api-test,$BACKEND || cat hugegraph-0.11.0/logs/hugegraph-server.log
 $TRAVIS_DIR/build-report.sh
 $TRAVIS_DIR/stop-server.sh
