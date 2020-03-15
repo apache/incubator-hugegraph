3af552805c0424a8782bbdb9d6befe354aba2eaf
Zhangmei Li <lizhangmei@baidu.com>
2020-03-16 00:57:26 +0800

    upgrade backend driver version
    Change-Id: I258bf91be896df48d19aac8194d632b1b22c30d0

--- hugegraph-core/src/main/java/com/baidu/hugegraph/backend/store/memory/InMemoryDBStoreProvider.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/backend/store/memory/InMemoryDBStoreProvider.java
@@ -92,6 +92,7 @@ public class InMemoryDBStoreProvider extends AbstractBackendStoreProvider {
          *                    rangeLong and rangeDouble
          * [1.4] #746: support userdata for indexlabel
          * [1.5] #820: store vertex properties in one column
+         * [1.6] #894: encode label id in string index
          */
         return "1.5";
     }
