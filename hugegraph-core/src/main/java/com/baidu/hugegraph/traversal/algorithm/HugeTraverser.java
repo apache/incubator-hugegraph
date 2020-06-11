ebe79aa2c81773648bedfcba5bad4b51a9b08bd3
Jermy Li <lizhangmei@baidu.com>
2020-06-11 14:58:39 +0800

    add ownedBy() for HugeTraverser.Path (#1028)
    * add ownedBy() for HugeTraverser.Path
    * log SnowflakeId with debug
    
    Change-Id: I38c74343037014ae0c4111966dad00ef3a72093a

--- hugegraph-core/src/main/java/com/baidu/hugegraph/traversal/algorithm/HugeTraverser.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/traversal/algorithm/HugeTraverser.java
@@ -452,11 +452,11 @@ public class HugeTraverser {
         if (sorted) {
             map = CollectionUtil.sortByValue(map, false);
         }
-        if (limit == NO_LIMIT) {
+        if (limit == NO_LIMIT || map.size() <= limit) {
             return map;
         }
         Map<K, V> results = InsertionOrderUtil.newMap();
-        long count = 0;
+        long count = 0L;
         for (Map.Entry<K, V> entry : map.entrySet()) {
             results.put(entry.getKey(), entry.getValue());
             if (++count >= limit) {
@@ -580,6 +580,17 @@ public class HugeTraverser {
             } else {
                 return ImmutableMap.of("objects", this.vertices);
             }
+        }
+
+        public boolean ownedBy(Id source) {
+            E.checkNotNull(source, "source");
+            Id min = null;
+            for (Id id : this.vertices) {
+                if (min == null || id.compareTo(min) < 0) {
+                    min = id;
+                }
+            }
+            return source.equals(min);
         }
 
         @Override
