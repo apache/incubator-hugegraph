924c5e174b626fba1be366c35c3713645cd28490
zhangyi51 <zhangyi51@baidu.com>
2020-09-16 11:53:38 +0800

    move jaccard similar into jaccard similarity
    Change-Id: Ia6ff5efd7e2a195f85427992b7bff875d30dec48

--- hugegraph-api/src/main/java/com/baidu/hugegraph/api/traversers/JaccardSimilarityAPI.java
+++ hugegraph-api/src/main/java/com/baidu/hugegraph/api/traversers/JaccardSimilarityAPI.java
@@ -19,11 +19,17 @@
 
 package com.baidu.hugegraph.api.traversers;
 
+import java.util.Map;
+
+import static com.baidu.hugegraph.traversal.algorithm.HugeTraverser.DEFAULT_CAPACITY;
 import static com.baidu.hugegraph.traversal.algorithm.HugeTraverser.DEFAULT_DEGREE;
+import static com.baidu.hugegraph.traversal.algorithm.HugeTraverser.DEFAULT_LIMIT;
 
 import javax.inject.Singleton;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -33,22 +39,26 @@ import javax.ws.rs.core.Context;
 import org.slf4j.Logger;
 
 import com.baidu.hugegraph.HugeGraph;
-import com.baidu.hugegraph.api.API;
 import com.baidu.hugegraph.api.graph.EdgeAPI;
 import com.baidu.hugegraph.api.graph.VertexAPI;
 import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.core.GraphManager;
 import com.baidu.hugegraph.server.RestServer;
+import com.baidu.hugegraph.structure.HugeVertex;
+import com.baidu.hugegraph.traversal.algorithm.EdgeStep;
 import com.baidu.hugegraph.traversal.algorithm.HugeTraverser;
+import com.baidu.hugegraph.traversal.algorithm.JaccardSimilarTraverser;
 import com.baidu.hugegraph.type.define.Directions;
+import com.baidu.hugegraph.util.E;
 import com.baidu.hugegraph.util.JsonUtil;
 import com.baidu.hugegraph.util.Log;
 import com.codahale.metrics.annotation.Timed;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableMap;
 
 @Path("graphs/{graph}/traversers/jaccardsimilarity")
 @Singleton
-public class JaccardSimilarityAPI extends API {
+public class JaccardSimilarityAPI extends TraverserAPI {
 
     private static final Logger LOG = Log.logger(RestServer.class);
 
@@ -77,5 +87,55 @@ public class JaccardSimilarityAPI extends API {
                                                         edgeLabel, degree);
         return JsonUtil.toJson(ImmutableMap.of("jaccard_similarity",
                                                similarity));
+    }
+
+    @POST
+    @Timed
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON_WITH_CHARSET)
+    public String post(@Context GraphManager manager,
+                       @PathParam("graph") String graph,
+                       Request request) {
+        E.checkArgumentNotNull(request, "The request body can't be null");
+        E.checkArgumentNotNull(request.vertex,
+                               "The source vertex of request can't be null");
+        E.checkArgument(request.step != null,
+                        "The steps of request can't be null");
+        E.checkArgument(request.top >= 0,
+                        "The top must be >= 0, but got: %s", request.top);
+
+        LOG.debug("Graph [{}] get jaccard similars from source vertex '{}', " +
+                  "with step '{}', top '{}' and capacity '{}'",
+                  graph, request.vertex, request.step, request.top);
+
+        HugeGraph g = graph(manager, graph);
+        Id sourceId = HugeVertex.getIdValue(request.vertex);
+
+        EdgeStep step = step(g, request.step);
+
+        JaccardSimilarTraverser traverser = new JaccardSimilarTraverser(g);
+        Map<Id, Double> results = traverser.jaccardSimilars(sourceId, step,
+                                                            request.top,
+                                                            request.capacity);
+        return manager.serializer(g).writeMap(results);
+    }
+
+    private static class Request {
+
+        @JsonProperty("vertex")
+        public Object vertex;
+        @JsonProperty("step")
+        public TraverserAPI.Step step;
+        @JsonProperty("top")
+        public int top = Integer.valueOf(DEFAULT_LIMIT);
+        @JsonProperty("capacity")
+        public long capacity = Long.valueOf(DEFAULT_CAPACITY);
+
+        @Override
+        public String toString() {
+            return String.format("Request{vertex=%s,step=%s,top=%s," +
+                                 "capacity=%s}", this.vertex, this.step,
+                                 this.top, this.capacity);
+        }
     }
 }
