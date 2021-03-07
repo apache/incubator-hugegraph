99130a3a9d53c337cde7f6622da5b841d679bf4d
Zhangmei Li <lizhangmei@baidu.com>
2021-03-08 00:50:16 +0800

    fix codecov missing some files coverage by jacococli
    Change-Id: Ica4ea713a9b74255c1bd4e5c0adbe233faeaceb2

--- hugegraph-dist/src/assembly/travis/build-report.sh
+++ hugegraph-dist/src/assembly/travis/build-report.sh
@@ -2,8 +2,17 @@
 
 set -ev
 
+OPTION_CLASS_FILES_BACKEND="--classfiles hugegraph-$BACKEND/target/classes/com/baidu/hugegraph"
+if [ "$BACKEND" == "memory" ]; then
+    # hugegraph-memory is the same as hugegraph-core
+    OPTION_CLASS_FILES_BACKEND=""
+fi
+
 cd hugegraph-test
 mvn jacoco:dump@pull-test-data -Dapp.host=localhost -Dapp.port=36320 -Dskip.dump=false
 cd ../
 java -jar $TRAVIS_DIR/jacococli.jar report hugegraph-test/target/jacoco-it.exec \
-     --classfiles hugegraph-api/target/classes/com/baidu/hugegraph --xml report.xml
+     --classfiles hugegraph-dist/target/classes/com/baidu/hugegraph \
+     --classfiles hugegraph-api/target/classes/com/baidu/hugegraph \
+     --classfiles hugegraph-core/target/classes/com/baidu/hugegraph \
+     $OPTION_CLASS_FILES_BACKEND --xml report.xml
