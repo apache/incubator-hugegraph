1a573b43d773f042836bb6abc31446f0f3daa0a2
Zhangmei Li <lizhangmei@baidu.com>
2021-04-17 21:16:03 +0800

    upgrade travis from ubuntu-14.04 to ubuntu-16.04
    Change-Id: I364d6409418c2c1a582e6c2a2b47d81184efe674

--- hugegraph-dist/src/assembly/travis/install-scylladb.sh
+++ hugegraph-dist/src/assembly/travis/install-scylladb.sh
@@ -3,16 +3,23 @@
 set -ev
 
 TRAVIS_DIR=`dirname $0`
-# reference: https://github.com/scylladb/gocqlx/commit/c7f0483dd30b1c7ad1972ea135528dc95cc4ce32
-DOWNLOAD_ADDRESS="http://s3.amazonaws.com/downloads.scylladb.com/deb/ubuntu"
+# reference:
+# https://www.scylladb.com/download/?platform=ubuntu-16.04&version=scylla-4.4#open-source
+# https://github.com/scylladb/gocqlx/commit/c7f0483dd30b1c7ad1972ea135528dc95cc4ce32
+DOWNLOAD_ADDRESS="http://downloads.scylladb.com/deb/ubuntu/scylla-4.4-$(lsb_release -s -c).list"
 SCYLLA_OPTS="--network-stack posix --enable-in-memory-data-store 1 --developer-mode 1"
 SCYLLA_OPTS_LOG="--log-to-stdout 1 --default-log-level info"
 
 # download and install scylladb
-echo "deb [arch=amd64] $DOWNLOAD_ADDRESS trusty scylladb-1.7/multiverse" | \
-    sudo tee -a /etc/apt/sources.list > /dev/null
-sudo apt-get -qq update
-sudo apt-get install -y --allow-unauthenticated scylla-server
+sudo apt-get install apt-transport-https
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 5e08fbd8b5d6ec9c
+sudo curl -L --output /etc/apt/sources.list.d/scylla.list $DOWNLOAD_ADDRESS
+
+sudo apt-get update
+sudo apt-get install scylla
+
+cat /etc/scylla/scylla.yaml
 
 # start scylladb service
-sudo /usr/bin/scylla --options-file /etc/scylla/scylla.yaml ${SCYLLA_OPTS} ${SCYLLA_OPTS_LOG} &
+#sudo /usr/bin/scylla --options-file /etc/scylla/scylla.yaml ${SCYLLA_OPTS} ${SCYLLA_OPTS_LOG} &
+sudo systemctl start scylla-server
