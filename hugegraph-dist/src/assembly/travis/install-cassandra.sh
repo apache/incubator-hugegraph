3541132b22071355a0082500bd7ea1e97087d35f
Zhangmei Li <lizhangmei@baidu.com>
2021-04-18 00:55:06 +0800

    debug: Job for scylla-server.service failed
    Change-Id: I70ef59c592b6af33062a00d74005d0a8c1fb56e4

--- hugegraph-dist/src/assembly/travis/install-cassandra.sh
+++ hugegraph-dist/src/assembly/travis/install-cassandra.sh
@@ -3,27 +3,28 @@
 set -ev
 
 TRAVIS_DIR=`dirname $0`
-CASSA_DOWNLOAD_ADDRESS="http://archive.apache.org/dist/cassandra"
-CASSA_VERSION="3.10"
-CASSA_PACKAGE="apache-cassandra-${CASSA_VERSION}"
-CASSA_TAR="${CASSA_PACKAGE}-bin.tar.gz"
+CASS_DOWNLOAD_ADDRESS="http://archive.apache.org/dist/cassandra"
+CASS_VERSION="3.10"
+CASS_PACKAGE="apache-cassandra-${CASS_VERSION}"
+CASS_TAR="${CASS_PACKAGE}-bin.tar.gz"
+CASS_CONF="${CASS_PACKAGE}/conf/cassandra.yaml"
 
 # download cassandra
-if [ ! -f $HOME/downloads/${CASSA_TAR} ]; then
-  wget -q -O $HOME/downloads/${CASSA_TAR} ${CASSA_DOWNLOAD_ADDRESS}/${CASSA_VERSION}/${CASSA_TAR}
+if [ ! -f $HOME/downloads/${CASS_TAR} ]; then
+  wget -q -O $HOME/downloads/${CASS_TAR} ${CASS_DOWNLOAD_ADDRESS}/${CASS_VERSION}/${CASS_TAR}
 fi
 
 # decompress cassandra
-cp $HOME/downloads/${CASSA_TAR} ${CASSA_TAR} && tar xzf ${CASSA_TAR}
+cp $HOME/downloads/${CASS_TAR} ${CASS_TAR} && tar -xzf ${CASS_TAR}
 
 # using tmpfs for the Cassandra data directory reduces travis test runtime
 sudo mkdir /mnt/ramdisk
 sudo mount -t tmpfs -o size=1024m tmpfs /mnt/ramdisk
-sudo ln -s /mnt/ramdisk $CASSA_PACKAGE/data
+sudo ln -s /mnt/ramdisk $CASS_PACKAGE/data
 
 # config cassandra
-sed -i "s/batch_size_warn_threshold_in_kb:.*/batch_size_warn_threshold_in_kb: 10240/g" ${CASSA_PACKAGE}/conf/cassandra.yaml
-sed -i "s/batch_size_fail_threshold_in_kb:.*/batch_size_fail_threshold_in_kb: 10240/g" ${CASSA_PACKAGE}/conf/cassandra.yaml
+sed -i "s/batch_size_warn_threshold_in_kb:.*/batch_size_warn_threshold_in_kb: 10240/g" $CASS_CONF
+sed -i "s/batch_size_fail_threshold_in_kb:.*/batch_size_fail_threshold_in_kb: 10240/g" $CASS_CONF
 
 # start cassandra service
-sh ${CASSA_PACKAGE}/bin/cassandra
+sh ${CASS_PACKAGE}/bin/cassandra
