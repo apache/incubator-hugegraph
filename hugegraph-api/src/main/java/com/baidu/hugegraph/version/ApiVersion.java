e267a530ec8259ce1fb19c17809932b8178ed4ec
imbajin <imbajin@users.noreply.github.com>
2021-05-11 21:44:28 +0800

    rename olap to oltp

--- hugegraph-api/src/main/java/com/baidu/hugegraph/version/ApiVersion.java
+++ hugegraph-api/src/main/java/com/baidu/hugegraph/version/ApiVersion.java
@@ -110,7 +110,7 @@ public final class ApiVersion {
      *        multi-node-shortest-path, jaccard-similar and template-paths
      * [0.59] Issue-1333: Support graph read mode for olap property
      * [0.60] Issue-1392: Support create and resume snapshot
-     * [0.61] Issue-1433: Unify naming of degree for olap algorithms
+     * [0.61] Issue-1433: Unify naming of degree for oltp algorithms
      */
 
     // The second parameter of Version.of() is for IDE running without JAR
