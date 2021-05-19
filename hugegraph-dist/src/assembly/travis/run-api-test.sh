d95d5ad69a173e1deeeeb498cf1fde0b014b980e
coderzc <zc1217zc@126.com>
2021-05-19 20:58:54 +0800

    fix shell

--- hugegraph-dist/src/assembly/travis/run-api-test.sh
+++ hugegraph-dist/src/assembly/travis/run-api-test.sh
@@ -11,9 +11,8 @@ GREMLIN_SERVER_CONF=$SERVER_DIR/conf/gremlin-server.yaml
 
 mvn package -DskipTests
 
-sed -i '$/#auth.authenticator=/auth.authenticator=com.baidu.hugegraph.auth
-.StandardAuthenticator/' $REST_SERVER_CONF
-sed -i '$/gremlin.graph=.*/gremlin.graph=com.baidu.hugegraph.auth.HugeFactoryAuthProxy/' $CONF
+sed -i 's/#auth.authenticator=/auth.authenticator=com.baidu.hugegraph.auth.StandardAuthenticator/' $REST_SERVER_CONF
+sed -i 's/gremlin.graph=.*/gremlin.graph=com.baidu.hugegraph.auth.HugeFactoryAuthProxy/' $CONF
 echo "
 authentication: {
   authenticator: com.baidu.hugegraph.auth.StandardAuthenticator,
