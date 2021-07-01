964f91160ec102bd951765e9ba6534ca90288420
imbajin <imbajin@users.noreply.github.com>
2021-07-01 14:44:41 +0800

    Update ci.yml

--- hugegraph-dist/src/assembly/travis/install-mysql-via-docker.sh
+++ hugegraph-dist/src/assembly/travis/install-mysql-via-docker.sh
@@ -8,9 +8,14 @@ TRAVIS_DIR=`dirname $0`
 CONF=hugegraph-test/src/main/resources/hugegraph.properties
 MYSQL_USERNAME=root
 MYSQL_PASSWORD=123456
+
 # Set MySQL configurations
 sed -i "s/jdbc.username=.*/jdbc.username=$MYSQL_USERNAME/" $CONF
 sed -i "s/jdbc.password=.*/jdbc.password=$MYSQL_PASSWORD/" $CONF
 
-docker pull mysql:5.7
-docker run -p 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7
+sed -i "s/jdbc.reconnect_max_times=.*/jdbc.reconnect_max_times=10/" $CONF
+sed -i "s/jdbc.reconnect_interval=.*/jdbc.reconnect_interval=5/" $CONF
+
+
+#docker pull mysql:5.7
+#docker run -p 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7
