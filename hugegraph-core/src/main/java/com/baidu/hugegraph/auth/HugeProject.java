c8ea317234f00291222c4ede5168408ed3c49efc
liningrui <liningrui@baidu.com>
2021-07-07 18:31:55 +0800

    fix small bug og HugeProject
    Change-Id: I538714f0b5ee3a8e90aa90db8f43f4b1cc0597d6

--- hugegraph-core/src/main/java/com/baidu/hugegraph/auth/HugeProject.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/auth/HugeProject.java
@@ -270,7 +270,7 @@ public class HugeProject extends Entity {
 
         @Override
         public void initSchemaIfNeeded() {
-            if (this.existEdgeLabel(this.label)) {
+            if (this.existVertexLabel(this.label)) {
                 return;
             }
 
