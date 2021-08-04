hong0220/hugegraph

support tikv 1st version
Change-Id: I23fd71ec977a1944a02ea726717fc8b3c8659a15

hugegraph-tikv/src/main/java/com/baidu/hugegraph/backend/store/tikv/TikvMetrics.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.baidu.hugegraph.backend.store.tikv;
+
+import java.util.Map;
+
+import com.baidu.hugegraph.backend.store.BackendMetrics;
+import com.baidu.hugegraph.util.E;
+
+public class TikvMetrics implements BackendMetrics {
+
+    public TikvMetrics(TikvSessions tikv) {
+        E.checkArgumentNotNull(tikv, "Tikv connection is not opened");
+    }
+
+    @Override
+    public Map<String, Object> getMetrics() {
+        return null;
+    }
+}

apache/incubator-hugegraph

hugegraph-tikv/src/main/java/com/baidu/hugegraph/backend/store/tikv/TikvMetrics.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.baidu.hugegraph.backend.store.tikv;
+
+import java.util.Map;
+
+import com.baidu.hugegraph.backend.store.BackendMetrics;
+import com.baidu.hugegraph.util.E;
+
+public class TikvMetrics implements BackendMetrics {
+
+    public TikvMetrics(TikvSessions tikv) {
+        E.checkArgumentNotNull(tikv, "Tikv connection is not opened");
+    }
+
+    @Override
+    public Map<String, Object> metrics() {
+        return null;
+    }
+}