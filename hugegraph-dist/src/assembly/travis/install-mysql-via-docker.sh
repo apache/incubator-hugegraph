46394f26711291f1c8a5bf3d226a09037ffd192c
imbajin <imbajin@users.noreply.github.com>
2021-11-10 16:03:11 +0800

    update

--- hugegraph-dist/src/assembly/travis/install-mysql-via-docker.sh
+++ hugegraph-dist/src/assembly/travis/install-mysql-via-docker.sh
@@ -7,15 +7,14 @@ TRAVIS_DIR=$(dirname "$0")
 # Need speed up it
 CONF=hugegraph-test/src/main/resources/hugegraph.properties
 MYSQL_USERNAME=root
-#MYSQL_PASSWORD=123456
 
 # Set MySQL configurations
 sed -i "s/jdbc.username=.*/jdbc.username=$MYSQL_USERNAME/" $CONF
-#sed -i "s/jdbc.password=.*/jdbc.password=$MYSQL_PASSWORD/" $CONF
 
 sed -i "s/jdbc.reconnect_max_times=.*/jdbc.reconnect_max_times=10/" $CONF
 sed -i "s/jdbc.reconnect_interval=.*/jdbc.reconnect_interval=5/" $CONF
 
 
+# Keep for upgrade in future
 #docker pull mysql:5.7
-#docker run -p 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7
+#docker run -p 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD="******" -d mysql:5.7
--- hugegraph-dist/src/assembly/travis/install-postgresql-via-docker.sh
+++ hugegraph-dist/src/assembly/travis/install-postgresql-via-docker.sh
@@ -8,13 +8,11 @@ CONF=hugegraph-test/src/main/resources/hugegraph.properties
 POSTGRESQL_DRIVER=org.postgresql.Driver
 POSTGRESQL_URL=jdbc:postgresql://localhost:5432/
 POSTGRESQL_USERNAME=postgres
-#POSTGRESQL_PASSWORD=123456
 
 # Set PostgreSQL configurations
 sed -i "s/jdbc.driver=.*/jdbc.driver=$POSTGRESQL_DRIVER/" $CONF
 sed -i "s?jdbc.url=.*?jdbc.url=$POSTGRESQL_URL?" $CONF
 sed -i "s/jdbc.username=.*/jdbc.username=$POSTGRESQL_USERNAME/" $CONF
-#sed -i "s/jdbc.password=.*/jdbc.password=$POSTGRESQL_PASSWORD/" $CONF
 
 sudo service postgresql stop 9.2
 
