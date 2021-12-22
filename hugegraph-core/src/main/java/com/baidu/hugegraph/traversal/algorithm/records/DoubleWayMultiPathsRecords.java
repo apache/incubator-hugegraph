tangyzer/hugegraph

add concurrent IntSet/IntMap
Change-Id: Ib11aebc16569be6ccc8169908d4bf1e4288800f3

hugegraph-core/src/main/java/com/baidu/hugegraph/traversal/algorithm/records/DoubleWayMultiPathsRecords.java
@@ -30,9 +30,9 @@ import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.perf.PerfUtil.Watched;
 import com.baidu.hugegraph.traversal.algorithm.HugeTraverser.Path;
 import com.baidu.hugegraph.traversal.algorithm.HugeTraverser.PathSet;
-import com.baidu.hugegraph.traversal.algorithm.records.record.IntIterator;
 import com.baidu.hugegraph.traversal.algorithm.records.record.Record;
 import com.baidu.hugegraph.traversal.algorithm.records.record.RecordType;
+import com.baidu.hugegraph.util.collection.IntIterator;
 import com.google.common.collect.Lists;
 
 public abstract class DoubleWayMultiPathsRecords extends AbstractRecords {
@@ -40,7 +40,7 @@ public abstract class DoubleWayMultiPathsRecords extends AbstractRecords {
     private final Stack<Record> sourceRecords;
     private final Stack<Record> targetRecords;
 
-    private IntIterator lastRecordKeys;
+    private Iterator<Integer> lastRecordKeys;
     private int currentKey;
     private boolean forward;
     private long accessed;

hugegraph-core/src/main/java/com/baidu/hugegraph/traversal/algorithm/records/SingleWayMultiPathsRecords.java
@@ -25,10 +25,6 @@ import java.util.List;
 import java.util.Stack;
 import java.util.function.Function;
 
-import org.eclipse.collections.api.set.primitive.MutableIntSet;
-import org.eclipse.collections.impl.map.mutable.primitive.IntIntHashMap;
-import org.eclipse.collections.impl.set.mutable.primitive.IntHashSet;
-
 import com.baidu.hugegraph.HugeException;
 import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.iterator.MapperIterator;
@@ -36,11 +32,12 @@ import com.baidu.hugegraph.perf.PerfUtil.Watched;
 import com.baidu.hugegraph.traversal.algorithm.HugeTraverser.Path;
 import com.baidu.hugegraph.traversal.algorithm.HugeTraverser.PathSet;
 import com.baidu.hugegraph.traversal.algorithm.records.record.Int2IntRecord;
-import com.baidu.hugegraph.traversal.algorithm.records.record.IntIterator;
 import com.baidu.hugegraph.traversal.algorithm.records.record.Record;
 import com.baidu.hugegraph.traversal.algorithm.records.record.RecordType;
 import com.baidu.hugegraph.type.define.CollectionType;
 import com.baidu.hugegraph.util.collection.CollectionFactory;
+import com.baidu.hugegraph.util.collection.IntMap;
+import com.baidu.hugegraph.util.collection.IntSet;
 
 public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
 
@@ -48,9 +45,9 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
 
     private final int sourceCode;
     private final boolean nearest;
-    private final MutableIntSet accessedVertices;
+    private final IntSet accessedVertices;
 
-    private IntIterator lastRecordKeys;
+    private Iterator<Integer> lastRecordKeys;
 
     public SingleWayMultiPathsRecords(RecordType type, boolean concurrent,
                                       Id source, boolean nearest) {
@@ -64,8 +61,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
         this.records = new Stack<>();
         this.records.push(firstRecord);
 
-        this.accessedVertices = concurrent ? new IntHashSet().asSynchronized() :
-                                new IntHashSet();
+        this.accessedVertices = CollectionFactory.newIntSet(CollectionType.EC);
     }
 
     @Override
@@ -94,7 +90,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
                             boolean all, boolean ring) {
         PathSet paths = new PathSet();
         for (int i = 1; i < this.records.size(); i++) {
-            IntIterator iterator = this.records.get(i).keys();
+            Iterator<Integer> iterator = this.records.get(i).keys();
             while (iterator.hasNext()) {
                 paths.add(this.linkPath(i, iterator.next()));
             }
@@ -130,7 +126,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
         // Find the layer where the target is located
         int foundLayer = -1;
         for (int i = 0; i < this.records.size(); i++) {
-            IntIntHashMap layer = this.layer(i);
+            IntMap layer = this.layer(i);
             if (!layer.containsKey(target)) {
                 continue;
             }
@@ -143,7 +139,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
         // If a layer found, then concat parents
         if (foundLayer > 0) {
             for (int i = foundLayer; i > 0; i--) {
-                IntIntHashMap layer = this.layer(i);
+                IntMap layer = this.layer(i);
                 // Uptrack parents
                 target = layer.get(target);
                 ids.add(this.id(target));
@@ -154,7 +150,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
 
     protected final Path linkPath(int layerIndex, int target) {
         List<Id> ids = CollectionFactory.newList(CollectionType.EC);
-        IntIntHashMap layer = this.layer(layerIndex);
+        IntMap layer = this.layer(layerIndex);
         if (!layer.containsKey(target)) {
             throw new HugeException("Failed to get path for %s",
                                     this.id(target));
@@ -172,9 +168,9 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
         return new Path(ids);
     }
 
-    protected final IntIntHashMap layer(int layerIndex) {
+    protected final IntMap layer(int layerIndex) {
         Record record = this.records.elementAt(layerIndex);
-        IntIntHashMap layer = ((Int2IntRecord) record).layer();
+        IntMap layer = ((Int2IntRecord) record).layer();
         return layer;
     }
 

apache/incubator-hugegraph

hugegraph-core/src/main/java/com/baidu/hugegraph/traversal/algorithm/records/DoubleWayMultiPathsRecords.java
@@ -30,9 +30,9 @@ import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.perf.PerfUtil.Watched;
 import com.baidu.hugegraph.traversal.algorithm.HugeTraverser.Path;
 import com.baidu.hugegraph.traversal.algorithm.HugeTraverser.PathSet;
-import com.baidu.hugegraph.traversal.algorithm.records.record.IntIterator;
 import com.baidu.hugegraph.traversal.algorithm.records.record.Record;
 import com.baidu.hugegraph.traversal.algorithm.records.record.RecordType;
+import com.baidu.hugegraph.util.collection.IntIterator;
 import com.google.common.collect.Lists;
 
 public abstract class DoubleWayMultiPathsRecords extends AbstractRecords {
@@ -40,7 +40,7 @@ public abstract class DoubleWayMultiPathsRecords extends AbstractRecords {
     private final Stack<Record> sourceRecords;
     private final Stack<Record> targetRecords;
 
-    private IntIterator parentRecordKeys;
+    private Iterator<Integer> parentRecordKeys;
     private int currentKey;
     private boolean movingForward;
     private long accessed;

hugegraph-core/src/main/java/com/baidu/hugegraph/traversal/algorithm/records/SingleWayMultiPathsRecords.java
@@ -25,10 +25,6 @@ import java.util.List;
 import java.util.Stack;
 import java.util.function.Function;
 
-import org.eclipse.collections.api.set.primitive.MutableIntSet;
-import org.eclipse.collections.impl.map.mutable.primitive.IntIntHashMap;
-import org.eclipse.collections.impl.set.mutable.primitive.IntHashSet;
-
 import com.baidu.hugegraph.HugeException;
 import com.baidu.hugegraph.backend.id.Id;
 import com.baidu.hugegraph.iterator.MapperIterator;
@@ -36,11 +32,12 @@ import com.baidu.hugegraph.perf.PerfUtil.Watched;
 import com.baidu.hugegraph.traversal.algorithm.HugeTraverser.Path;
 import com.baidu.hugegraph.traversal.algorithm.HugeTraverser.PathSet;
 import com.baidu.hugegraph.traversal.algorithm.records.record.Int2IntRecord;
-import com.baidu.hugegraph.traversal.algorithm.records.record.IntIterator;
 import com.baidu.hugegraph.traversal.algorithm.records.record.Record;
 import com.baidu.hugegraph.traversal.algorithm.records.record.RecordType;
 import com.baidu.hugegraph.type.define.CollectionType;
 import com.baidu.hugegraph.util.collection.CollectionFactory;
+import com.baidu.hugegraph.util.collection.IntMap;
+import com.baidu.hugegraph.util.collection.IntSet;
 
 public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
 
@@ -48,9 +45,9 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
 
     private final int sourceCode;
     private final boolean nearest;
-    private final MutableIntSet accessedVertices;
+    private final IntSet accessedVertices;
 
-    private IntIterator parentRecordKeys;
+    private Iterator<Integer> parentRecordKeys;
 
     public SingleWayMultiPathsRecords(RecordType type, boolean concurrent,
                                       Id source, boolean nearest) {
@@ -64,8 +61,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
         this.records = new Stack<>();
         this.records.push(firstRecord);
 
-        this.accessedVertices = concurrent ? new IntHashSet().asSynchronized() :
-                                new IntHashSet();
+        this.accessedVertices = CollectionFactory.newIntSet(CollectionType.EC);
     }
 
     @Override
@@ -95,7 +91,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
                             boolean all, boolean ring) {
         PathSet paths = new PathSet();
         for (int i = 1; i < this.records.size(); i++) {
-            IntIterator iterator = this.records.get(i).keys();
+            Iterator<Integer> iterator = this.records.get(i).keys();
             while (iterator.hasNext()) {
                 paths.add(this.linkPath(i, iterator.next()));
             }
@@ -131,7 +127,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
         // Find the layer where the target is located
         int foundLayer = -1;
         for (int i = 0; i < this.records.size(); i++) {
-            IntIntHashMap layer = this.layer(i);
+            IntMap layer = this.layer(i);
             if (!layer.containsKey(target)) {
                 continue;
             }
@@ -144,7 +140,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
         // If a layer found, then concat parents
         if (foundLayer > 0) {
             for (int i = foundLayer; i > 0; i--) {
-                IntIntHashMap layer = this.layer(i);
+                IntMap layer = this.layer(i);
                 // Uptrack parents
                 target = layer.get(target);
                 ids.add(this.id(target));
@@ -155,7 +151,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
 
     protected final Path linkPath(int layerIndex, int target) {
         List<Id> ids = CollectionFactory.newList(CollectionType.EC);
-        IntIntHashMap layer = this.layer(layerIndex);
+        IntMap layer = this.layer(layerIndex);
         if (!layer.containsKey(target)) {
             throw new HugeException("Failed to get path for %s",
                                     this.id(target));
@@ -173,7 +169,7 @@ public abstract class SingleWayMultiPathsRecords extends AbstractRecords {
         return new Path(ids);
     }
 
-    protected final IntIntHashMap layer(int layerIndex) {
+    protected final IntMap layer(int layerIndex) {
         Record record = this.records.elementAt(layerIndex);
         return ((Int2IntRecord) record).layer();
     }