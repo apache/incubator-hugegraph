chentianlong1993/hugegraph

merge gremlin-server graphs and specified graphs
Change-Id: I5626be6a0c56074c51339283bf6f6e40914ae56b

hugegraph-dist/src/main/java/com/baidu/hugegraph/cmd/InitStore.java
@@ -23,13 +23,14 @@ import java.util.Collection;
 import java.util.Map;
 
 import org.apache.commons.collections.map.MultiValueMap;
-import org.apache.tinkerpop.gremlin.util.config.YamlConfiguration;
 import org.slf4j.Logger;
 
 import com.baidu.hugegraph.HugeFactory;
 import com.baidu.hugegraph.HugeGraph;
 import com.baidu.hugegraph.auth.StandardAuthenticator;
 import com.baidu.hugegraph.backend.store.BackendStoreSystemInfo;
+import com.baidu.hugegraph.config.HugeConfig;
+import com.baidu.hugegraph.config.ServerOptions;
 import com.baidu.hugegraph.dist.RegisterUtil;
 import com.baidu.hugegraph.util.ConfigUtil;
 import com.baidu.hugegraph.util.E;
@@ -39,7 +40,6 @@ public class InitStore {
 
     private static final Logger LOG = Log.logger(InitStore.class);
 
-    private static final String GRAPHS = "graphs";
     // 6~8 retries may be needed under high load for Cassandra backend
     private static final int RETRIES = 10;
     // Less than 5000 may cause mismatch exception with Cassandra backend
@@ -57,39 +57,40 @@ public class InitStore {
     }
 
     public static void main(String[] args) throws Exception {
-        E.checkArgument(args.length == 3,
-                        "HugeGraph init-store need to pass 3 parameters, " +
-                        "they are: the config files of gremlin-server and " +
-                        "rest-server, and the config directory of graphs. " +
-                        "For example: conf/gremlin-server.yaml " +
-                        "conf/rest-server.properties conf/graphs");
+        E.checkArgument(args.length == 2,
+                        "HugeGraph init-store need to pass 2 parameters, " +
+                        "they are the config files of GremlinServer and " +
+                        "RestServer, for example: conf/gremlin-server.yaml " +
+                        "conf/rest-server.properties");
         E.checkArgument(args[0].endsWith(".yaml"),
                         "Expect the 1st parameter is yaml config file.");
         E.checkArgument(args[1].endsWith(".properties"),
                         "Expect the 2nd parameter is properties config file.");
 
-        String gremlinConfFile = args[0];
-        String restConfFile = args[1];
-        String graphsDir = args[2];
+        String gremlinConf = args[0];
+        String restConf = args[1];
 
         RegisterUtil.registerBackends();
         RegisterUtil.registerPlugins();
         RegisterUtil.registerServer();
 
-        YamlConfiguration yamlConfig = new YamlConfiguration();
-        yamlConfig.load(gremlinConfFile);
+        HugeConfig restServerConfig = new HugeConfig(restConf);
+        String graphsDir = restServerConfig.get(ServerOptions.GRAPHS);
 
-        E.checkArgument(yamlConfig.getRootNode().getChildren(GRAPHS).isEmpty(),
-                        "Don't allow to specify '%s' node in config file '%s'",
-                        GRAPHS, gremlinConfFile);
+        Map<String, String> gremlinGraphs = ConfigUtil.parseGremlinGraphs(
+                                            gremlinConf);
+        Map<String, String> graphs = ConfigUtil.scanGraphsDir(graphsDir);
+        Map<String, String> mergedGraphs = ConfigUtil.mergeGraphConfs(
+                                           gremlinGraphs, graphs);
 
-        Map<String, String> graphConfs = ConfigUtil.scanGraphsDir(graphsDir);
-        for (Map.Entry<String, String> entry : graphConfs.entrySet()) {
-            HugeFactory.checkGraphName(entry.getKey(), "gremlin-server.yaml");
+        for (Map.Entry<String, String> entry : mergedGraphs.entrySet()) {
             initGraph(entry.getValue());
         }
+        // In order to RestServer can read all the graphs
+        // TODO: after copied, next merge will trigger conflict
+        ConfigUtil.copyFiles(graphsDir, gremlinGraphs);
 
-        StandardAuthenticator.initAdminUser(restConfFile);
+        StandardAuthenticator.initAdminUser(restConf);
 
         HugeFactory.shutdown(30L);
     }

apache/incubator-hugegraph

hugegraph-dist/src/main/java/com/baidu/hugegraph/cmd/InitStore.java
@@ -24,7 +24,6 @@ import java.util.Map;
 
 import org.apache.commons.collections.map.MultiValueMap;
 import org.apache.tinkerpop.gremlin.structure.util.GraphFactory;
-import org.apache.tinkerpop.gremlin.util.config.YamlConfiguration;
 import org.slf4j.Logger;
 
 import com.baidu.hugegraph.HugeFactory;
@@ -33,6 +32,7 @@ import com.baidu.hugegraph.auth.StandardAuthenticator;
 import com.baidu.hugegraph.backend.store.BackendStoreSystemInfo;
 import com.baidu.hugegraph.config.CoreOptions;
 import com.baidu.hugegraph.config.HugeConfig;
+import com.baidu.hugegraph.config.ServerOptions;
 import com.baidu.hugegraph.dist.RegisterUtil;
 import com.baidu.hugegraph.util.ConfigUtil;
 import com.baidu.hugegraph.util.E;
@@ -42,7 +42,6 @@ public class InitStore {
 
     private static final Logger LOG = Log.logger(InitStore.class);
 
-    private static final String GRAPHS = "graphs";
     // 6~8 retries may be needed under high load for Cassandra backend
     private static final int RETRIES = 10;
     // Less than 5000 may cause mismatch exception with Cassandra backend
@@ -60,39 +59,40 @@ public class InitStore {
     }
 
     public static void main(String[] args) throws Exception {
-        E.checkArgument(args.length == 3,
-                        "HugeGraph init-store need to pass 3 parameters, " +
-                        "they are: the config files of gremlin-server and " +
-                        "rest-server, and the config directory of graphs. " +
-                        "For example: conf/gremlin-server.yaml " +
-                        "conf/rest-server.properties conf/graphs");
+        E.checkArgument(args.length == 2,
+                        "HugeGraph init-store need to pass 2 parameters, " +
+                        "they are the config files of GremlinServer and " +
+                        "RestServer, for example: conf/gremlin-server.yaml " +
+                        "conf/rest-server.properties");
         E.checkArgument(args[0].endsWith(".yaml"),
                         "Expect the 1st parameter is yaml config file");
         E.checkArgument(args[1].endsWith(".properties"),
                         "Expect the 2nd parameter is properties config file");
 
-        String gremlinConfFile = args[0];
-        String restConfFile = args[1];
-        String graphsDir = args[2];
+        String gremlinConf = args[0];
+        String restConf = args[1];
 
         RegisterUtil.registerBackends();
         RegisterUtil.registerPlugins();
         RegisterUtil.registerServer();
 
-        YamlConfiguration yamlConfig = new YamlConfiguration();
-        yamlConfig.load(gremlinConfFile);
+        HugeConfig restServerConfig = new HugeConfig(restConf);
+        String graphsDir = restServerConfig.get(ServerOptions.GRAPHS);
 
-        E.checkArgument(yamlConfig.getRootNode().getChildren(GRAPHS).isEmpty(),
-                        "Don't allow to specify '%s' node in config file '%s'",
-                        GRAPHS, gremlinConfFile);
+        Map<String, String> gremlinGraphs = ConfigUtil.parseGremlinGraphs(
+                                            gremlinConf);
+        Map<String, String> graphs = ConfigUtil.scanGraphsDir(graphsDir);
+        Map<String, String> mergedGraphs = ConfigUtil.mergeGraphConfs(
+                                           gremlinGraphs, graphs);
 
-        Map<String, String> graphConfs = ConfigUtil.scanGraphsDir(graphsDir);
-        for (Map.Entry<String, String> entry : graphConfs.entrySet()) {
-            HugeFactory.checkGraphName(entry.getKey(), "gremlin-server.yaml");
+        for (Map.Entry<String, String> entry : mergedGraphs.entrySet()) {
             initGraph(entry.getValue());
         }
+        // In order to RestServer can read all the graphs
+        // TODO: after copied, next merge will trigger conflict
+        ConfigUtil.copyFiles(graphsDir, gremlinGraphs);
 
-        StandardAuthenticator.initAdminUserIfNeeded(restConfFile);
+        StandardAuthenticator.initAdminUserIfNeeded(restConf);
 
         HugeFactory.shutdown(30L);
     }