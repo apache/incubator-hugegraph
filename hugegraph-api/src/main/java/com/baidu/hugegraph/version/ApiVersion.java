14f33e08982cae881be18bb4ef37ff4ae93fbcd6
guoygang <hero.gariker@gmail.com>
2022-01-10 15:29:36 +0800

    upgrade version from 0.13 to 3.0 (#166)
    * upgrade version from 0.13 to 3.0

--- hugegraph-api/src/main/java/com/baidu/hugegraph/version/ApiVersion.java
+++ hugegraph-api/src/main/java/com/baidu/hugegraph/version/ApiVersion.java
@@ -122,13 +122,14 @@ public final class ApiVersion {
      * version 0.13:
      * [0.67] Issue-11: hugegraph with etcd/pd,
      *        add graphspace, remove auth server
+     * [3.0] version 3.0.0
      */
 
     // The second parameter of Version.of() is for IDE running without JAR
-    public static final Version VERSION = Version.of(ApiVersion.class, "0.68");
+    public static final Version VERSION = Version.of(ApiVersion.class, "3.0");
 
     public static final void check() {
         // Check version of hugegraph-core. Firstly do check from version 0.3
-        VersionUtil.check(CoreVersion.VERSION, "0.13", "0.14", CoreVersion.NAME);
+        VersionUtil.check(CoreVersion.VERSION, "3.0", "3.1", CoreVersion.NAME);
     }
 }
