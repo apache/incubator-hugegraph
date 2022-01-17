280dc6801d37830bf33152cf34b2ebd9ce40c2e8
guoygang <hero.gariker@gmail.com>
2022-01-17 14:28:33 +0800

    remove default graph (#188)

--- hugegraph-dist/src/assembly/travis/run-api-test.sh
+++ hugegraph-dist/src/assembly/travis/run-api-test.sh
@@ -7,14 +7,11 @@ BACKEND=$1
 TRAVIS_DIR=`dirname $0`
 VERSION=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
 SERVER_DIR=hugegraph-$VERSION
-CONF=$SERVER_DIR/conf/graphs/hugegraph.properties
 REST_SERVER_CONF=$SERVER_DIR/conf/rest-server.properties
 GREMLIN_SERVER_CONF=$SERVER_DIR/conf/gremlin-server.yaml
 
 mvn package -DskipTests
-
-# config hugegraph.properties
-# sed -i 's/gremlin.graph=.*/gremlin.graph=com.baidu.hugegraph.auth.HugeFactoryAuthProxy/' $CONF
+cp $TRAVIS_DIR/graphs/hugegraph.properties $SERVER_DIR/conf/graphs/
 
 $TRAVIS_DIR/start-server.sh $SERVER_DIR $BACKEND || (cat $SERVER_DIR/logs/hugegraph-server.log && exit 1)
 
