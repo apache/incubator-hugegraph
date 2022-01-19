e0b9fbd67ec850e7a7e328e9918b5d91163e6f51
guoygang <hero.gariker@gmail.com>
2022-01-19 15:58:42 +0800

    修复获取图schema遗漏enableLabelIndex信息 (#202)

--- hugegraph-core/src/main/java/com/baidu/hugegraph/schema/VertexLabel.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/schema/VertexLabel.java
@@ -188,6 +188,8 @@ public class VertexLabel extends SchemaLabel {
         // Enable label index
         if (this.enableLabelIndex()) {
             builder.append(".enableLabelIndex(true)");
+        } else {
+            builder.append(".enableLabelIndex(false)");
         }
 
         // User data
