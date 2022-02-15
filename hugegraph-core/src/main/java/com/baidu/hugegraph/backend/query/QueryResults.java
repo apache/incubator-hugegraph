72baa123b135240476718f735c085577b761a970
Jermy Li <lizhangmei@baidu.com>
2022-02-15 13:55:26 +0800

    improve CachedGraphTransaction perf (#1743)
    Change-Id: I9f60f68d2faedb099e89adc2b1c1f4948d177a40

--- hugegraph-core/src/main/java/com/baidu/hugegraph/backend/query/QueryResults.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/backend/query/QueryResults.java
@@ -22,7 +22,6 @@ package com.baidu.hugegraph.backend.query;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
