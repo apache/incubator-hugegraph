3ef5d3816bbb41d1eb244054a5116504444f4cf1
AisinGiorro <54138330+sunlujing@users.noreply.github.com>
2022-03-12 21:11:26 +0800

    Update CachedGraphTransaction.java
    1. fix the bug when vertex updated, but the edge cache associated with that vertex was not updated. Here we simply clear all edge cache when  any vertex changes.

--- hugegraph-core/src/main/java/com/baidu/hugegraph/backend/cache/CachedGraphTransaction.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/backend/cache/CachedGraphTransaction.java
@@ -398,7 +398,7 @@ public final class CachedGraphTransaction extends GraphTransaction {
             // for vertex change, the edge associated with that vertex should also be updated
             // here we just clear all the edge cache , before we use a more precise strategy
             boolean invalidEdgesCache = (this.edgesInTxSize() + updates.size() + deletions.size()) > 0;
-            if (invalidEdgesCache) {
+            if (invalidEdgesCache && this.enableCacheEdge()) {
                 // TODO: Use a more precise strategy to update the edge cache
                 this.edgesCache.clear();
                 this.notifyChanges(Cache.ACTION_CLEARED, HugeType.EDGE, null);
