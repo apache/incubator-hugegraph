39125fe6ddceefa5a60aa625c1807aa508468fec
zhaoxiangxin <zhaoxiangxin@MacintoshdeMacBook-Pro.local>
2022-03-17 11:57:07 +0800

    modify default partitions to 2*workers

--- hugegraph-core/src/main/java/com/baidu/hugegraph/job/ComputerDisJob.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/job/ComputerDisJob.java
@@ -132,7 +132,7 @@ public class ComputerDisJob extends UserJob<Object> {
         int worker = Integer.parseInt(String.valueOf(map.get("worker")));
 
         Map<String, String> k8sParams = new HashMap<>();
-        k8sParams.put("job.partitions_count", String.valueOf(worker * 10));
+        k8sParams.put("job.partitions_count", String.valueOf(worker * 2));
         for (Map.Entry<String, Object> item : params.entrySet()) {
             k8sParams.put(item.getKey(), item.getValue().toString());
         }
