db1fa5401f5d810ae9f8ec6b30da6b2f05c50e3c
z7658329 <1043706593@qq.com>
2022-04-18 22:55:44 +0800

    fix cassandra code checkstyle

--- hugegraph-cassandra/src/main/java/com/baidu/hugegraph/backend/store/cassandra/CassandraSessionPool.java
+++ hugegraph-cassandra/src/main/java/com/baidu/hugegraph/backend/store/cassandra/CassandraSessionPool.java
@@ -206,7 +206,9 @@ public class CassandraSessionPool extends BackendSessionPool {
             assert this.session == null;
             try {
                 this.open();
-            } catch (InvalidQueryException ignored) {}
+            } catch (InvalidQueryException ignored) {
+                // ignore
+            }
         }
 
         @Override
