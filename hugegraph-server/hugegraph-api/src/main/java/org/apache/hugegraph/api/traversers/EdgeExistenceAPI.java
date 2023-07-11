5db1cfbf30d882454d3ce620dc7ceb256836a342
M <1216063060@qq.com>
2023-07-12 00:09:07 +0800

    feat: optimising adjacency edge queries

---
+++ hugegraph-server/hugegraph-api/src/main/java/org/apache/hugegraph/api/traversers/EdgeExistenceAPI.java
@@ -0,0 +1,57 @@
+package org.apache.hugegraph.api.traversers;
+
+import com.codahale.metrics.annotation.Timed;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import org.apache.hugegraph.HugeGraph;
+import org.apache.hugegraph.backend.id.Id;
+import org.apache.hugegraph.core.GraphManager;
+import org.apache.hugegraph.structure.HugeVertex;
+import org.apache.hugegraph.traversal.algorithm.EdgeExistenceTraverser;
+import org.apache.hugegraph.util.E;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+@Path("graphs/{graph}/traversers/edgeexistence")
+@Singleton
+@Tag(name = "EdgeExistenceAPI")
+public class EdgeExistenceAPI extends TraverserAPI {
+
+    @GET
+    @Timed
+    @Produces(APPLICATION_JSON_WITH_CHARSET)
+    public String get(
+        @Context GraphManager manager,
+        @PathParam("graph") String graph,
+        @QueryParam("source") String source,
+        @QueryParam("target") String target,
+        @QueryParam("edgelabel")
+        @DefaultValue("BOTH") String edgeLabel,
+        @QueryParam("limit")
+        @DefaultValue("1000") long limit) {
+
+        E.checkArgumentNotNull(source, "The source can't be null");
+        E.checkArgumentNotNull(source, "The target can't be null");
+
+        Id sourceId = HugeVertex.getIdValue(source);
+        Id targetId = HugeVertex.getIdValue(target);
+        HugeGraph hugeGraph = graph(manager, graph);
+        EdgeExistenceTraverser traverser = new EdgeExistenceTraverser(hugeGraph);
+
+        Iterator<Edge> edges = traverser.queryEdgeExistence(sourceId, targetId, edgeLabel, limit);
+
+        List<Id> all = new ArrayList<>();
+        while (edges.hasNext()) {
+            all.add((Id) edges.next().id());
+        }
+        return manager.serializer(hugeGraph).writeList("EdgeIds", all);
+    }
+
+
+}
---
+++ hugegraph-server/hugegraph-core/src/main/java/org/apache/hugegraph/traversal/algorithm/EdgeExistenceTraverser.java
@@ -0,0 +1,48 @@
+package org.apache.hugegraph.traversal.algorithm;
+
+import org.apache.hugegraph.HugeGraph;
+import org.apache.hugegraph.backend.id.Id;
+import org.apache.hugegraph.backend.query.ConditionQuery;
+import org.apache.hugegraph.type.HugeType;
+import org.apache.hugegraph.type.define.Directions;
+import org.apache.hugegraph.type.define.HugeKeys;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class EdgeExistenceTraverser extends HugeTraverser {
+    public EdgeExistenceTraverser(HugeGraph graph) {
+        super(graph);
+    }
+
+    public Iterator<Edge> queryEdgeExistence(
+        Id sourceId, Id targetId,
+        String label, long limit) {
+        if ("BOTH".equals(label)) {
+            return queryByNeighbor(sourceId, targetId, limit);
+        }
+        ConditionQuery conditionQuery = new ConditionQuery(HugeType.EDGE);
+        conditionQuery.eq(HugeKeys.OWNER_VERTEX, sourceId);
+        conditionQuery.eq(HugeKeys.OTHER_VERTEX, targetId);
+        conditionQuery.eq(HugeKeys.LABEL, getEdgeLabelId(label));
+        conditionQuery.eq(HugeKeys.DIRECTION, Directions.OUT);
+        conditionQuery.eq(HugeKeys.SORT_VALUES, "");
+
+        return graph().edges(conditionQuery);
+    }
+
+    private Iterator<Edge> queryByNeighbor(Id sourceId, Id targetId, long limit) {
+        Iterator<Edge> edges = this.edgesOfVertex(sourceId, Directions.OUT, (Id) null, limit);
+        List<Edge> res = new ArrayList<>();
+        String target = targetId.toString();
+        while (edges.hasNext()) {
+            Edge edge = edges.next();
+            String outVertexId = edge.inVertex().id().toString();
+            if (!target.equals(outVertexId)) continue;
+            res.add(edge);
+        }
+        return res.iterator();
+    }
+}
