4c78e32966cad01cca26b0e1b4e591c37fc07192
VGalaxies <dyc1904821183@gmail.com>
2023-10-11 00:12:52 +0800

    chore: prepare hstore backend UT

--- hugegraph-server/hugegraph-dist/src/assembly/travis/install-hstore.sh
+++ hugegraph-server/hugegraph-dist/src/assembly/travis/install-hstore.sh
@@ -17,38 +17,4 @@
 #
 set -ev
 
-if [[ $# -ne 1 ]]; then
-    echo "Must pass BACKEND type of hugegraph"
-    exit 1
-fi
-
-BACKEND=$1
-TRAVIS_DIR=$(dirname "$0")
-
-if [ ! -d "$HOME"/downloads ]; then
-    mkdir "$HOME"/downloads
-fi
-
-case $BACKEND in
-    cassandra)
-        "$TRAVIS_DIR"/install-cassandra.sh
-        ;;
-    scylladb)
-        "$TRAVIS_DIR"/install-scylladb.sh
-        ;;
-    hbase)
-        "$TRAVIS_DIR"/install-hbase.sh
-        ;;
-    mysql)
-        "$TRAVIS_DIR"/install-mysql-via-docker.sh
-        ;;
-    postgresql)
-        "$TRAVIS_DIR"/install-postgresql-via-docker.sh
-        ;;
-    hstore)
-        "$TRAVIS_DIR"/install-hstore.sh
-        ;;
-    *)
-        # don't need to install for other backends
-        ;;
-esac
+# TODO
