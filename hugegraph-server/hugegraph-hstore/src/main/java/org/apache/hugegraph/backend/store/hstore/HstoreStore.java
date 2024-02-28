1fce458dd5043ac0b705de3a8333df1bd95dd32d
sheli00 <sheli9726@foxmail.com>
2024-02-28 16:37:41 +0800

    style: return old style

--- hugegraph-server/hugegraph-hstore/src/main/java/org/apache/hugegraph/backend/store/hstore/HstoreStore.java
+++ hugegraph-server/hugegraph-hstore/src/main/java/org/apache/hugegraph/backend/store/hstore/HstoreStore.java
@@ -396,8 +396,8 @@ public abstract class HstoreStore extends AbstractBackendStore<Session> {
     //                for (ConditionQuery conditionQuery :
     //                    ConditionQueryFlatten.flatten(cq)) {
     //                    Id label = conditionQuery.condition(HugeKeys.LABEL);
-    //                 /* 父类型 + sortKeys：g.V("V.id").outE("parentLabel").has
-    //                 ("sortKey","value") 转成 所有子类型 + sortKeys*/
+    //                 /* 父类型 + sortKeys： g.V("V.id").outE("parentLabel").has
+    //                 ("sortKey","value")转成 所有子类型 + sortKeys*/
     //                    if ((this.subEls == null ||
     //                         !this.subEls.hasNext()) && label != null &&
     //                        hugeGraph.edgeLabel(label).isFather() &&
@@ -563,7 +563,7 @@ public abstract class HstoreStore extends AbstractBackendStore<Session> {
         if (this.isGraphStore && !olapPks.isEmpty()) {
             List<Iterator<BackendEntry>> iterators = new ArrayList<>();
             for (Id pk : olapPks) {
-                // 构造 olap 表查询 query condition
+                // 构造olap表查询query condition
                 Query q = this.constructOlapQueryCondition(pk, query);
                 table = this.table(HugeType.OLAP);
                 iterators.add(table.queryOlap(this.session(HugeType.OLAP), q));
@@ -576,9 +576,9 @@ public abstract class HstoreStore extends AbstractBackendStore<Session> {
 
 
     /**
-     * 重新构造 查询 olap 表 query
-     * 由于 olap 合并成一张表，在写入 olap 数据，key 在后面增加了 pk
-     * 所以在此进行查询的时候，需要重新构造 pk 前缀
+     * 重新构造 查询olap表 query
+     * 由于 olap合并成一张表, 在写入olap数据, key在后面增加了pk
+     * 所以在此进行查询的时候,需要重新构造pk前缀
      * 写入参考 BinarySerializer.writeOlapVertex
      *
      * @param pk
