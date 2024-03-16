b16d510d34478696a323f020273794c8b03f6a2a
VGalaxies <vgalaxies@apache.org>
2024-03-16 17:07:45 +0800

    rearrange test package

--- hugegraph-pd/hg-pd-test/src/main/java/org/apache/hugegraph/pd/core/MetadataKeyHelperTest.java
+++ hugegraph-pd/hg-pd-test/src/main/java/org/apache/hugegraph/pd/core/MetadataKeyHelperTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.hugegraph.pd.common;
+package org.apache.hugegraph.pd.core;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
