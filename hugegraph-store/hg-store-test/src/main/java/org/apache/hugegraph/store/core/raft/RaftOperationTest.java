7ff2e2093b6c5f661235baa7758fe2bba1f0570b
Peng Junzhi <201250214@smail.nju.edu.cn>
2024-06-23 12:17:34 +0800

    feat(store): integrate rest of store-test submodule

---
+++ hugegraph-store/hg-store-test/src/main/java/org/apache/hugegraph/store/core/raft/RaftOperationTest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hugegraph.store.core.raft;
+
+import org.apache.hugegraph.pd.grpc.Metapb;
+import org.apache.hugegraph.store.raft.RaftOperation;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.protobuf.GeneratedMessageV3;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class RaftOperationTest {
+
+    private RaftOperation raftOperationUnderTest;
+
+    @Before
+    public void setUp() {
+        raftOperationUnderTest = new RaftOperation();
+    }
+
+
+    @Test
+    public void testCreate1() {
+        // Run the test
+        final RaftOperation result = RaftOperation.create((byte) 0b0);
+        assertEquals(null, result.getReq());
+        assertEquals((byte) 0b0, result.getOp());
+    }
+
+    @Test
+    public void testCreate2() {
+        // Run the test
+        final RaftOperation result = RaftOperation.create((byte) 0b0, "content".getBytes(), "req");
+        assertArrayEquals("content".getBytes(), result.getValues());
+        assertEquals("req", result.getReq());
+        assertEquals((byte) 0b0, result.getOp());
+    }
+
+    @Test
+    public void testCreate3() {
+        // Run the test
+        final RaftOperation result = RaftOperation.create((byte) 0b0, "req");
+        assertEquals("req", result.getReq());
+        assertEquals((byte) 0b0, result.getOp());
+    }
+
+    @Test
+    public void testCreate4() throws Exception {
+        // Setup
+        final GeneratedMessageV3 req = Metapb.Graph.newBuilder().setGraphName("name").build();
+
+        // Run the test
+        final RaftOperation result = RaftOperation.create((byte) 0b0, req);
+        assertEquals((byte) 0b0, result.getOp());
+
+    }
+}
