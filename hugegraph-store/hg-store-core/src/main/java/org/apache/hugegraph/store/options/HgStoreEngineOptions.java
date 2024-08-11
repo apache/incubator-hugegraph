a038d2341cc58cb66af34e270d6134b56e54389f
YangJiaqi <yang.jiaqi.hadoop@gmail.com>
2024-08-11 13:04:34 +0800

    fix(hstore): JRaft maxEntriesSize configuration parameters do not take effect (#2630)
    Co-authored-by: imbajin <jin@apache.org>

--- hugegraph-store/hg-store-core/src/main/java/org/apache/hugegraph/store/options/HgStoreEngineOptions.java
+++ hugegraph-store/hg-store-core/src/main/java/org/apache/hugegraph/store/options/HgStoreEngineOptions.java
@@ -98,7 +98,7 @@ public class HgStoreEngineOptions {
         /**
          * The maximum number of entries in AppendEntriesRequest
          */
-        private final int maxEntriesSize = 256;
+        private int maxEntriesSize = 256;
         /**
          * Raft cluster data backlog occurs, rate limiting wait time in milliseconds.
          **/
