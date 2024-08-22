c5d9d77430243a8b113d95ba21ac63e6f7843bee
JackyYangPassion <yang.jiaqi.hadoop@gmail.com>
2024-08-22 13:56:55 +0800

    increment sleep 10s

--- hugegraph-server/hugegraph-test/src/main/java/org/apache/hugegraph/core/TaskCoreTest.java
+++ hugegraph-server/hugegraph-test/src/main/java/org/apache/hugegraph/core/TaskCoreTest.java
@@ -686,7 +686,7 @@ public class TaskCoreTest extends BaseCoreTest {
     }
 
     private static void sleepAWhile() {
-        sleepAWhile(100);
+        sleepAWhile(10000);
     }
 
     private static void sleepAWhile(long ms) {
