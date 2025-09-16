e0965334861b2697ecefa53b818661e2ad696311
JisoLya <523420504@qq.com>
2025-09-16 13:55:54 +0800

    update(store): update SystemMetricService

--- hugegraph-store/hg-store-core/src/main/java/org/apache/hugegraph/store/metric/SystemMetricService.java
+++ hugegraph-store/hg-store-core/src/main/java/org/apache/hugegraph/store/metric/SystemMetricService.java
@@ -17,10 +17,8 @@
 
 package org.apache.hugegraph.store.metric;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryUsage;
 import java.net.NetworkInterface;
@@ -37,7 +35,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.apache.hugegraph.rocksdb.access.RocksDBFactory;
 import org.apache.hugegraph.rocksdb.access.RocksDBSession;
@@ -78,10 +75,10 @@ public class SystemMetricService {
             loadDiskInfo(systemMetrics);
 
             // disk io
-            loadDiskIo(systemMetrics);
-
+            //loadDiskIo(systemMetrics);
+            //
             // network
-            loadNetFlowInfo(systemMetrics);
+            //loadNetFlowInfo(systemMetrics);
 
             // rocksdb
             loadRocksDbInfo(systemMetrics);
@@ -134,25 +131,25 @@ public class SystemMetricService {
         map.put("disk.usable_size", usable / MIB);
     }
 
-    private void loadDiskIo(Map<String, Long> map) {
-        for (Map.Entry<String, Float> entry : getDiskIoData().entrySet()) {
-            map.put(entry.getKey(), entry.getValue().longValue());
-        }
-    }
-
-    private void loadNetFlowInfo(Map<String, Long> map) {
-        for (Map.Entry<String, List<Long>> entry : getTraffic().entrySet()) {
-            // exclude none-functional network interface
-            map.put("network." + entry.getKey() + ".sent_bytes",
-                    entry.getValue().get(0) / 1024 / 1024);
-            map.put("network." + entry.getKey() + ".recv_bytes",
-                    entry.getValue().get(1) / 1024 / 1024);
-            map.put("network." + entry.getKey() + ".sent_rates",
-                    entry.getValue().get(2) / 1024 / 1024);
-            map.put("network." + entry.getKey() + ".recv_rates",
-                    entry.getValue().get(3) / 1024 / 1024);
-        }
-    }
+    //private void loadDiskIo(Map<String, Long> map) {
+    //    for (Map.Entry<String, Float> entry : getDiskIoData().entrySet()) {
+    //        map.put(entry.getKey(), entry.getValue().longValue());
+    //    }
+    //}
+    //
+    //private void loadNetFlowInfo(Map<String, Long> map) {
+    //    for (Map.Entry<String, List<Long>> entry : getTraffic().entrySet()) {
+    //        // exclude none-functional network interface
+    //        map.put("network." + entry.getKey() + ".sent_bytes", entry.getValue().get(0) / 1024
+    //        / 1024);
+    //        map.put("network." + entry.getKey() + ".recv_bytes", entry.getValue().get(1) / 1024
+    //        / 1024);
+    //        map.put("network." + entry.getKey() + ".sent_rates", entry.getValue().get(2) / 1024
+    //        / 1024);
+    //        map.put("network." + entry.getKey() + ".recv_rates", entry.getValue().get(3) / 1024
+    //        / 1024);
+    //    }
+    //}
 
     private void loadRocksDbInfo(Map<String, Long> map) {
         Map<MemoryUsageType, Long> dbMem =
@@ -205,89 +202,89 @@ public class SystemMetricService {
      *
      * @return
      */
-    private Map<String, List<Long>> getTraffic() {
-        deque.add(loadTrafficData());
-
-        if (deque.size() < 2) {
-            return new HashMap<>();
-        }
-        // keep 2 copies
-        while (deque.size() > 2) {
-            deque.removeFirst();
-        }
-
-        // compare
-        Map<String, List<Long>> result = new HashMap<>();
-        Map<String, List<Long>> currentFlows = deque.getLast();
-        Map<String, List<Long>> preFlows = deque.getFirst();
-
-        for (Map.Entry<String, List<Long>> entry : currentFlows.entrySet()) {
-            if (preFlows.containsKey(entry.getKey())) {
-                List<Long> prev = preFlows.get(entry.getKey());
-                List<Long> now = preFlows.get(entry.getKey());
-                // no traffic
-                if (now.get(0) == 0) {
-                    continue;
-                }
-                long diff = now.get(2) - prev.get(2);
-                diff = diff > 0 ? diff : 1L;
-                result.put(
-                        entry.getKey(),
-                        Arrays.asList(
-                                now.get(0) - prev.get(0),
-                                now.get(1) - prev.get(1),
-                                // rate rate
-                                (now.get(0) - prev.get(0)) / diff,
-                                // recv rate
-                                (now.get(1) - prev.get(1)) / diff));
-            }
-        }
-        return result;
-    }
+    //private Map<String, List<Long>> getTraffic() {
+    //    deque.add(loadTrafficData());
+    //
+    //    if (deque.size() < 2) {
+    //        return new HashMap<>();
+    //    }
+    //    // keep 2 copies
+    //    while (deque.size() > 2) {
+    //        deque.removeFirst();
+    //    }
+    //
+    //    // compare
+    //    Map<String, List<Long>> result = new HashMap<>();
+    //    Map<String, List<Long>> currentFlows = deque.getLast();
+    //    Map<String, List<Long>> preFlows = deque.getFirst();
+    //
+    //    for (Map.Entry<String, List<Long>> entry : currentFlows.entrySet()) {
+    //        if (preFlows.containsKey(entry.getKey())) {
+    //            List<Long> prev = preFlows.get(entry.getKey());
+    //            List<Long> now = preFlows.get(entry.getKey());
+    //            // no traffic
+    //            if (now.get(0) == 0) {
+    //                continue;
+    //            }
+    //            long diff = now.get(2) - prev.get(2);
+    //            diff = diff > 0 ? diff : 1L;
+    //            result.put(
+    //                entry.getKey(),
+    //                Arrays.asList(
+    //                    now.get(0) - prev.get(0),
+    //                    now.get(1) - prev.get(1),
+    //                    // rate rate
+    //                    (now.get(0) - prev.get(0)) / diff,
+    //                    // recv rate
+    //                    (now.get(1) - prev.get(1)) / diff));
+    //        }
+    //    }
+    //    return result;
+    //}
 
     /**
      * load traffic according to os, now only support mac os and linux
      *
      * @return
      */
-    private Map<String, List<Long>> loadTrafficData() {
-        String osName = System.getProperty("os.name").toLowerCase();
-        if (osName.startsWith("linux")) {
-            return loadLinuxTrafficData();
-        } else if (osName.startsWith("mac")) {
-            return loadMacOsTrafficData();
-        }
-        return new HashMap<>();
-    }
+    //private Map<String, List<Long>> loadTrafficData() {
+    //    String osName = System.getProperty("os.name").toLowerCase();
+    //    if (osName.startsWith("linux")) {
+    //        return loadLinuxTrafficData();
+    //    } else if (osName.startsWith("mac")) {
+    //        return loadMacOsTrafficData();
+    //    }
+    //    return new HashMap<>();
+    //}
 
     /**
      * read the result of "netstat -ib". (lo is ignored)
      *
      * @return
      */
-    private Map<String, List<Long>> loadMacOsTrafficData() {
-        Map<String, List<Long>> flows = new HashMap<>();
-        Long current = System.currentTimeMillis() / 1000;
-        for (String line : executeCmd("netstat -ib")) {
-            if (line.startsWith("Name") || line.startsWith("lo")) {
-                // first table header line
-                continue;
-            }
-
-            List<String> arr = Arrays.stream(line.split(" ")).filter(x -> x.length() > 0)
-                                     .collect(Collectors.toList());
-
-            long sentBytes = Long.parseLong(arr.get(arr.size() - 2));
-            long recvBytes = Long.parseLong(arr.get(arr.size() - 5));
-            String name = arr.get(0);
-            // log.debug("mac: {}, -> {},{},{}", line, sentBytes, recvBytes, name);
-            if (sentBytes > 0 && recvBytes > 0) {
-                flows.put(name, Arrays.asList(sentBytes, recvBytes, current));
-            }
-        }
-
-        return flows;
-    }
+    //private Map<String, List<Long>> loadMacOsTrafficData() {
+    //    Map<String, List<Long>> flows = new HashMap<>();
+    //    Long current = System.currentTimeMillis() / 1000;
+    //    for (String line : executeCmd("netstat -ib")) {
+    //        if (line.startsWith("Name") || line.startsWith("lo")) {
+    //            // first table header line
+    //            continue;
+    //        }
+    //
+    //        List<String> arr = Arrays.stream(line.split(" ")).filter(x -> x.length() > 0)
+    //        .collect(Collectors.toList());
+    //
+    //        long sentBytes = Long.parseLong(arr.get(arr.size() - 2));
+    //        long recvBytes = Long.parseLong(arr.get(arr.size() - 5));
+    //        String name = arr.get(0);
+    //        // log.debug("mac: {}, -> {},{},{}", line, sentBytes, recvBytes, name);
+    //        if (sentBytes > 0 && recvBytes > 0) {
+    //            flows.put(name, Arrays.asList(sentBytes, recvBytes, current));
+    //        }
+    //    }
+    //
+    //    return flows;
+    //}
 
     /**
      * read the statistics file for network interface
@@ -345,82 +342,82 @@ public class SystemMetricService {
         return names;
     }
 
-    private Map<String, Float> getDiskIoData() {
-        String osName = System.getProperty("os.name").toLowerCase();
-        if (osName.startsWith("linux")) {
-            return loadLinuxDiskIoData();
-        } else if (osName.startsWith("mac")) {
-            return loadMacDiskIoData();
-        }
-        return new HashMap<>();
-    }
+    //private Map<String, Float> getDiskIoData() {
+    //    String osName = System.getProperty("os.name").toLowerCase();
+    //    if (osName.startsWith("linux")) {
+    //        return loadLinuxDiskIoData();
+    //    } else if (osName.startsWith("mac")) {
+    //        return loadMacDiskIoData();
+    //    }
+    //    return new HashMap<>();
+    //}
 
     /**
      * get io data using iostat -d -x -k
      *
      * @return
      */
-    private Map<String, Float> loadLinuxDiskIoData() {
-        Map<String, Float> result = new HashMap<>();
-        boolean contentFlag = false;
-        for (String line : executeCmd("iostat -d -x -k")) {
-            // header
-            if (line.startsWith("Device")) {
-                contentFlag = true;
-                continue;
-            }
-
-            if (contentFlag) {
-                List<String> arr =
-                        Arrays.stream(line.split(" ")).filter(x -> x.length() > 0)
-                              .collect(Collectors.toList());
-                try {
-                    // util%
-                    result.put("disk.io." + arr.get(0) + ".util",
-                               Float.valueOf(arr.get(arr.size() - 1)) * 100);
-                    // wait
-                    result.put("disk.io." + arr.get(0) + ".wait",
-                               Float.valueOf(arr.get(arr.size() - 5)) * 100);
-                } catch (Exception e) {
-                    log.debug("error get disk io data {}", line);
-                }
-            }
-        }
-        return result;
-    }
+    //private Map<String, Float> loadLinuxDiskIoData() {
+    //    Map<String, Float> result = new HashMap<>();
+    //    boolean contentFlag = false;
+    //    for (String line : executeCmd("iostat -d -x -k")) {
+    //        // header
+    //        if (line.startsWith("Device")) {
+    //            contentFlag = true;
+    //            continue;
+    //        }
+    //
+    //        if (contentFlag) {
+    //            List<String> arr =
+    //                Arrays.stream(line.split(" ")).filter(x -> x.length() > 0).collect
+    //                (Collectors.toList());
+    //            try {
+    //                // util%
+    //                result.put("disk.io." + arr.get(0) + ".util", Float.valueOf(arr.get(arr
+    //                .size() - 1)) * 100);
+    //                // wait
+    //                result.put("disk.io." + arr.get(0) + ".wait", Float.valueOf(arr.get(arr
+    //                .size() - 5)) * 100);
+    //            } catch (Exception e) {
+    //                log.debug("error get disk io data {}", line);
+    //            }
+    //        }
+    //    }
+    //    return result;
+    //}
 
     /**
      * get io data using iostat
      *
      * @return
      */
-    private Map<String, Float> loadMacDiskIoData() {
-
-        Map<String, Float> result = new HashMap<>();
-        List<String> lines = executeCmd("iostat -oK");
-        // disks
-        List<String> disks =
-                Arrays.stream(lines.get(0).split(" "))
-                      .filter(x -> x.length() > 0 && x.startsWith("disk"))
-                      .collect(Collectors.toList());
-        // datas
-        List<String> data =
-                Arrays.stream(lines.get(2).split(" ")).filter(x -> x.length() > 0)
-                      .collect(Collectors.toList());
-        // zip data
-        for (int i = 0; i < disks.size(); i++) {
-            try {
-                // msps
-                result.put("disk.io." + disks.get(i) + ".wait",
-                           Float.valueOf(data.get(i * 3 + 2)) * 100);
-                // no such value
-                result.put("disk.io." + disks.get(i) + ".util", 0.0F);
-            } catch (Exception e) {
-                log.debug("error get io data {}", data.get(i));
-            }
-        }
-        return result;
-    }
+    //private Map<String, Float> loadMacDiskIoData() {
+    //
+    //    Map<String, Float> result = new HashMap<>();
+    //    List<String> lines = executeCmd("iostat -oK");
+    //    // disks
+    //    List<String> disks =
+    //        Arrays.stream(lines.get(0).split(" "))
+    //            .filter(x -> x.length() > 0 && x.startsWith("disk"))
+    //            .collect(Collectors.toList());
+    //    // datas
+    //    List<String> data =
+    //        Arrays.stream(lines.get(2).split(" ")).filter(x -> x.length() > 0).collect
+    //        (Collectors.toList());
+    //    // zip data
+    //    for (int i = 0; i < disks.size(); i++) {
+    //        try {
+    //            // msps
+    //            result.put("disk.io." + disks.get(i) + ".wait", Float.valueOf(data.get(i * 3 +
+    //            2)) * 100);
+    //            // no such value
+    //            result.put("disk.io." + disks.get(i) + ".util", 0.0F);
+    //        } catch (Exception e) {
+    //            log.debug("error get io data {}", data.get(i));
+    //        }
+    //    }
+    //    return result;
+    //}
 
     /**
      * execute cmd and get the output
@@ -428,21 +425,21 @@ public class SystemMetricService {
      * @param cmd
      * @return
      */
-    private List<String> executeCmd(String cmd) {
-        List<String> result = new ArrayList<>();
-        try {
-            Process pr = Runtime.getRuntime().exec(cmd);
-            BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                if (line.length() > 0) {
-                    result.add(line);
-                }
-            }
-            pr.waitFor();
-            in.close();
-        } catch (IOException | InterruptedException e) {
-        }
-        return result;
-    }
+    //private List<String> executeCmd(String cmd) {
+    //    List<String> result = new ArrayList<>();
+    //    try {
+    //        Process pr = Runtime.getRuntime().exec(cmd);
+    //        BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+    //        String line;
+    //        while ((line = in.readLine()) != null) {
+    //            if (line.length() > 0) {
+    //                result.add(line);
+    //            }
+    //        }
+    //        pr.waitFor();
+    //        in.close();
+    //    } catch (IOException | InterruptedException e) {
+    //    }
+    //    return result;
+    //}
 }
